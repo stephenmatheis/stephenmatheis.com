0924e0b84910ce6211698e07d8072183ff1f54f3
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-05-15 11:45:52 -0500

    Pages -> App

---
+++ app/not-found.jsx
@@ -0,0 +1,17 @@
+export default function NotFound() {
+    return (
+        <div
+            style={{
+                height: '100dvh',
+                width: '100vw',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignCenter: 'center',
+            }}
+        >
+            <h1 style={{ textAlign: 'center' }}>404</h1>
+            <h3 style={{ textAlign: 'center' }}>Page not found</h3>
+        </div>
+    );
+}
