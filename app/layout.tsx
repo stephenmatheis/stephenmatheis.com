0355db3534f812a43b2ff0a77abc88fdcc888569
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-05-18 18:40:55 -0500

    Basic MD blog setup (WIP)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from 'next';
 import '@/styles/globals.scss';
+
+export const metadata: Metadata = {
+    title: 'Stephen Matheis',
+    description: "Stephen Matheis' portfolio, resume, and blog.",
+    viewport: 'width=device-width, initial-scale=1,  viewport-fit=cover',
+    icons: 'manifest.json',
+};
 
 export default function RootLayout({
     children,
@@ -7,30 +15,6 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <head>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1,  viewport-fit=cover"
-                />
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="favicon/apple-touch-icon.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="favicon/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="favicon/favicon-16x16.png"
-                />
-                <link rel="manifest" href="site.webmanifest" />
-            </head>
             <body suppressHydrationWarning>{children}</body>
         </html>
     );
