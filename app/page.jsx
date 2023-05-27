62f71e421617e95ba4b5f0c9eccd33910ff5ac7b
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-05-26 21:56:26 -0500

    feat: replace resume at root with placeholder

--- app/page.jsx
+++ app/page.jsx
@@ -1,41 +1,7 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import classNames from 'classnames';
-import Header from '@/components/header/header';
-import Main from '@/components/main/main';
-import Footer from '@/components/footer';
-import styles from './resume.module.scss';
-
-export default function Home() {
-    const [loading, setLoading] = useState(true);
-    const [fade, setFade] = useState(false);
-    const speed = 70;
-
-    useEffect(() => {
-        if (!loading) {
-            document.body.classList.remove('hidden');
-            setFade(true);
-        } else {
-            document.body.classList.add('hidden');
-        }
-
-        return () => document.body.removeAttribute('style');
-    }, [loading]);
-
+export default function Page() {
     return (
-        <div
-            id={styles['resume']}
-            className={classNames({ [styles['loading']]: loading })}
-        >
-            <Header
-                speed={speed}
-                showLinkBackground={fade}
-                loading={loading}
-                setLoading={setLoading}
-            />
-            <Main loading={loading} speed={speed} fade={fade} />
-            <Footer />
+        <div className={{}}>
+            <h1>page.jsx</h1>
         </div>
     );
 }
