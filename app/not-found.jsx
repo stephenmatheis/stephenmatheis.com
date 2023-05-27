198a684f46d510e4ef8bcc56c1a9848241486a55
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-05-27 04:13:44 -0500

    fix: not found page (404) for dynamic routes

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,17 +1,26 @@
+import LinkCtr from '@/components/link-ctr';
+
 export default function NotFound() {
     return (
         <div
             style={{
                 height: '100dvh',
                 width: '100vw',
+                paddingTop: '90px',
                 display: 'flex',
                 flexDirection: 'column',
-                justifyContent: 'center',
                 alignCenter: 'center',
             }}
         >
-            <h1 style={{ textAlign: 'center' }}>404</h1>
-            <h3 style={{ textAlign: 'center' }}>Page not found</h3>
+            <h1 style={{ textAlign: 'center' }}>404 - Page not found</h1>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <LinkCtr text="Go back to Home" href="/" />
+            </div>
         </div>
     );
 }
