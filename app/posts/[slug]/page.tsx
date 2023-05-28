f22a36b5582298c50d56086fc0d2bf6561d753b1
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-05-28 00:19:00 -0500

    fix: remove post layout
    Fixes an issue where navigating to the a post page at `/posts/[slug]` might cause the page to load scrolled part of the way down. Apparently, layouts wrap their children in a div with a data attribute that next looks for to determine where to scroll to on load (or render?). Layouts are also not re-rendered. I assume this means that next couldn't figure out where the right scroll position should be if you navigate to post, go back, and select a different post since all post pages shared a nested layout.
    
    GH issue comments for reference:
    
    https://github.com/vercel/next.js/issues/42492#issuecomment-1320993510
    https://github.com/vercel/next.js/issues/42492#issuecomment-1345544864

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,26 +1,80 @@
-import getPosts, { getPost } from '@/lib/get-posts';
-import Body from './components/body';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
+import getPosts from '@/lib/get-posts';
+import Date from '@/components/date';
+import Nav from './components/nav';
+import Body from './components/body';
+import styles from './layout.module.scss';
+import '@/styles/globals.scss';
 
 export async function generateStaticParams() {
     const posts = await getPosts();
-    // The params to pre-render the page with.
-    // Without this, the page will be rendered at runtime
-    return posts.map((post: { slug: string }) => ({
-        params: { slug: post.slug },
-    }));
+
+    return posts.map((post) => ({ slug: post.slug }));
 }
 
-export default async function Page({
+export const generateMetadata = async ({
     params,
 }: {
     params: {
         slug: string;
     };
-}) {
-    const post = await getPost(params.slug);
+}): Promise<Metadata> => {
+    const post = (await getPosts()).find((p) => p?.slug === params.slug);
+
+    return {
+        title: post?.title,
+        description: post?.description,
+        alternates: {
+            canonical: `https://stephenmatheis.com/posts/${params.slug}`,
+        },
+    };
+};
+
+async function getData({ slug }: { slug: string }) {
+    const posts = await getPosts();
+    const postIndex = posts.findIndex((p) => p?.slug === slug);
+    const post = posts[postIndex];
+
+    if (!post) {
+        return undefined;
+    }
+
+    const { ...rest } = post;
+
+    return {
+        previous: posts[postIndex + 1] || null,
+        next: posts[postIndex - 1] || null,
+        ...rest,
+    };
+}
+
+type PageProps = {
+    params: {
+        slug: string;
+    };
+};
+
+export default async function Page({ params }: PageProps) {
+    const post = await getData(params);
 
     if (!post) return notFound();
 
-    return <Body>{post?.body}</Body>;
+    const { previous, next, title, body, date, lastModified } = post;
+
+    return (
+        <main className={styles.layout}>
+            <h1>{title}</h1>
+            <article>
+                <Date className={styles.date} dateString={date} />
+                <Body>{body}</Body>
+            </article>
+            {lastModified && (
+                <div className={styles.date}>
+                    Last modified <Date dateString={lastModified} />
+                </div>
+            )}
+            <Nav previous={previous} next={next} />
+        </main>
+    );
 }
