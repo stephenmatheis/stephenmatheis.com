b690a09ad5c908d55ffaa09ceb2cdc4dbed8544a
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-05-27 22:14:34 -0500

    feat: add title comment to posts page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,6 +2,7 @@ import PostsList from './components/posts-list';
 import getPosts from '@/lib/get-posts';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import Comment from '@/components/comment';
 import styles from './page.module.scss';
 
 export default async function Blog() {
@@ -11,6 +12,7 @@ export default async function Blog() {
         <div className={styles.page}>
             <Header />
             <main>
+                <Comment text="Posts" />
                 <PostsList posts={posts} />
             </main>
             <Footer links={[{ label: 'Home', path: '/' }]} />
