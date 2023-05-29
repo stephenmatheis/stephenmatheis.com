1239fd0e53f02f873514a55856529727da576ff5
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-05-29 13:12:05 -0500

    feat: move posts page main content to separate component

---
+++ app/posts/components/main/main.tsx
@@ -0,0 +1,15 @@
+import PostsList from '../posts-list';
+import getPosts from '@/lib/get-posts';
+import Comment from '@/components/comment';
+import styles from './main.module.scss';
+
+export default async function Main({}) {
+    const posts = await getPosts();
+
+    return (
+        <main className={styles.main}>
+            <Comment text="Posts" />
+            <PostsList posts={posts} />
+        </main>
+    );
+}
