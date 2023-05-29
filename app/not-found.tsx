e623e38c76478188e5565360cbcf8acb0c05b674
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-05-29 00:55:23 -0500

    fix: refactor .js and .jsx files to .ts and .tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+import LinkCtr from '@/components/link-ctr';
+
+export default function NotFound() {
+    return (
+        <div
+            style={{
+                height: '100dvh',
+                width: '100vw',
+                display: 'grid',
+                placeItems: 'center',
+            }}
+        >
+            <div style={{ display: 'grid', placeItems: 'center', gap: 36 }}>
+                <h1 style={{ textAlign: 'center', margin: 0 }}>
+                    404 - Page not found
+                </h1>
+                <LinkCtr text="Go back to Home" href="/" />
+            </div>
+        </div>
+    );
+}
