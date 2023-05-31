b595e14609197b69bd17dd02cba296e64bc16642
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-05-30 20:39:55 -0500

    feat: replace bright with rehype-pretty-code

--- app/posts/[slug]/components/body/body.tsx
+++ app/posts/[slug]/components/body/body.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { MDXRemote } from 'next-mdx-remote/rsc';
-import { Code } from 'bright';
+import rehypePrettyCode from 'rehype-pretty-code';
 
 // import remarkGfm from 'remark-gfm'
 // import rehypeSlug from 'rehype-slug'
@@ -29,40 +29,70 @@ const components = {
         );
     },
     img: ResponsiveImage,
-    pre: ({ children, ...props }: any) => {
-        Code.theme = 'light-plus';
-
-        return (
-            // https://github.com/vercel/next.js/issues/43537#issuecomment-1331054397
-            // Related issue: https://github.com/microsoft/TypeScript/pull/51328
-            // Next.js Doc: https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#async-and-await-in-server-components
-            // @ts-expect-error Async Server Component
-            <Code lineNumbers {...props}>
-                {children}
-            </Code>
-        );
-    },
 };
 
 export default function Body({ children }: { children: string }) {
+    const options = {
+        // Use one of Shiki's packaged themes
+        theme: 'dark-plus',
+        // Or your own JSON theme
+        // theme: JSON.parse(
+        //     fs.readFileSync(require.resolve('./themes/dark.json'), 'utf-8')
+        // ),
+
+        // Keep the background or use a custom background color?
+        keepBackground: false,
+
+        // Callback hooks to add custom logic to nodes when visiting
+        // them.
+        onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and
+            // allow empty lines to be copy/pasted
+            if (node.children.length === 0) {
+                node.children = [{ type: 'text', value: ' ' }];
+            }
+        },
+        onVisitHighlightedLine(node) {
+            // Each line node by default has `class="line"`.
+            node.properties.className.push('highlighted');
+        },
+        onVisitHighlightedWord(node, id) {
+            // Each word node has no className by default.
+            node.properties.className = ['word'];
+
+            if (id) {
+                // If the word spans across syntax boundaries (e.g. punctuation), remove
+                // colors from the child nodes.
+                if (node.properties['data-rehype-pretty-code-wrapper']) {
+                    node.children.forEach((childNode) => {
+                        childNode.properties.style = '';
+                    });
+                }
+
+                node.properties.style = '';
+                node.properties['data-word-id'] = id;
+            }
+        },
+    };
+
     return (
         // @ts-expect-error RSC
         <MDXRemote
             source={children}
-            //   options={{
-            //     mdxOptions: {
-            //       remarkPlugins: [
-            //         // Adds support for GitHub Flavored Markdown
-            //         remarkGfm,
-            //         // Makes emojis more accessible
-            //         remarkA11yEmoji,
-            //         // generates a table of contents based on headings
-            //         remarkToc,
-            //       ],
-            //       // These work together to add IDs and linkify headings
-            //       rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
-            //     },
-            //   }}
+            options={{
+                mdxOptions: {
+                    //   remarkPlugins: [
+                    //     // Adds support for GitHub Flavored Markdown
+                    //     remarkGfm,
+                    //     // Makes emojis more accessible
+                    //     remarkA11yEmoji,
+                    //     // generates a table of contents based on headings
+                    //     remarkToc,
+                    //   ],
+                    // These work together to add IDs and linkify headings
+                    rehypePlugins: [[rehypePrettyCode, options]],
+                },
+            }}
             components={components}
         />
     );
