a18f7fa986dcbf97a022009fa6ebee362664a7a8
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-05-31 22:20:42 -0500

    feat: projects mobile layout

--- components/projects/projects.tsx
+++ components/projects/projects.tsx
@@ -19,33 +19,39 @@ export default function Projects({ displayImages }: Props) {
     return (
         <div className={styles['projects']}>
             <Comment text={'Projects'} />
-            {projects.map(({ name, link, description, image }, index) => {
-                const { src, width, height }: ImageProps = image;
+            <div className={styles['projects-wrapper']}>
+                {projects.map(({ name, link, description, image }, index) => {
+                    const { src, width, height }: ImageProps = image;
 
-                return (
-                    <div
-                        key={index}
-                        className={classNames(styles['project-ctr'], {
-                            [styles['display-images']]: displayImages,
-                        })}
-                    >
-                        <LinkCtr href={link}>{name}</LinkCtr>
-                        {displayImages && src && (
-                            <div className={styles['img-ctr']}>
-                                <a href={link} target="_blank">
-                                    <Image
-                                        src={src}
-                                        alt={description}
-                                        width={width}
-                                        height={height}
-                                    />
-                                </a>
+                    return (
+                        <div
+                            key={index}
+                            className={classNames(styles['project-ctr'], {
+                                [styles['display-images']]: displayImages,
+                            })}
+                        >
+                            <LinkCtr href={link}>{name}</LinkCtr>
+                            <div className={styles['description-ctr']}>
+                                {displayImages && src && (
+                                    <div className={styles['img-ctr']}>
+                                        <a href={link} target="_blank">
+                                            <Image
+                                                src={src}
+                                                alt={description}
+                                                width={width}
+                                                height={height}
+                                            />
+                                        </a>
+                                    </div>
+                                )}
+                                <div className={styles.description}>
+                                    {description}
+                                </div>
                             </div>
-                        )}
-                        <div className={styles.description}>{description}</div>
-                    </div>
-                );
-            })}
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
