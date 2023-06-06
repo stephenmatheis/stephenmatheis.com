37041989d6e694d505f94083fa282607125a8ee1
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-06-05 21:27:03 -0500

    feat: replace root page main component (WIP)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { Page } from '@/components/page';
-import { Main } from './components/main';
+import { Main } from '@/components/main';
+import { LinkCtr } from '@/components/link-ctr';
+import styles from './page.module.scss';
 
 export default function RootPage() {
     return (
@@ -12,7 +14,30 @@ export default function RootPage() {
                 },
             ]}
         >
-            <Main />
+            <Main>
+                <section className={styles.left}>
+                    <nav>
+                        <LinkCtr href="/timeline">Timeline</LinkCtr>
+                        <LinkCtr href="/posts">Posts</LinkCtr>
+                        <LinkCtr href="/projects">Projects</LinkCtr>
+                        <LinkCtr href="/resume">About</LinkCtr>
+                    </nav>
+                </section>
+                <section className={styles.middle}>
+                    <video
+                        autoPlay
+                        loop
+                        playsInline
+                        muted
+                        preload="auto"
+                        style={{ width: 640 / 2, height: 480 / 2 }}
+                        src="/memoji-loop-22s.mov"
+                    />
+                </section>
+                <section className={styles.right}>
+                    {/* Intentionally empty to center `section.middle`. */}
+                </section>
+            </Main>
         </Page>
     );
 }
