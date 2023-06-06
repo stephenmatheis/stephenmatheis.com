5693c09a3f28f58195d5370bc6e6b170a5adc921
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-06-05 21:36:24 -0500

    feat: add sidebar (WIP)

---
+++ components/sidebar/sidebar.tsx
@@ -0,0 +1,13 @@
+import { LinkCtr } from '@/components/link-ctr';
+import styles from './sidebar.module.scss';
+
+export function Sidebar({}) {
+    return (
+        <nav className={styles.sidebar}>
+            <LinkCtr href="/timeline">Timeline</LinkCtr>
+            <LinkCtr href="/posts">Posts</LinkCtr>
+            <LinkCtr href="/projects">Projects</LinkCtr>
+            <LinkCtr href="/resume">About</LinkCtr>
+        </nav>
+    );
+}
