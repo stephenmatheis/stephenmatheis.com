8f66165079675c88cad7c6f73121857ad885daff
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-06-08 22:01:19 -0500

    feat: timeline layout

--- components/main/main.tsx
+++ components/main/main.tsx
@@ -2,12 +2,19 @@ import classNames from 'classnames';
 import { Sidebar } from '@/components/sidebar';
 import styles from './main.module.scss';
 
-type Props = { children: React.ReactNode; columns?: number };
+type Props = {
+    children: React.ReactNode;
+    columns?: number;
+    className?: string;
+    right?: {
+        className: string;
+    };
+};
 
-export function Main({ children, columns = 2 }: Props) {
+export function Main({ children, columns = 2, className, right }: Props) {
     return (
         <main
-            className={classNames(styles.main, {
+            className={classNames(styles.main, className, {
                 [styles['col-2']]: columns === 2,
                 [styles['col-3']]: columns === 3,
             })}
@@ -16,7 +23,9 @@ export function Main({ children, columns = 2 }: Props) {
                 <Sidebar />
             </section>
             {columns === 2 && (
-                <section className={styles.right}>{children}</section>
+                <section className={classNames(styles.right, right?.className)}>
+                    {children}
+                </section>
             )}
             {columns === 3 && (
                 <>
