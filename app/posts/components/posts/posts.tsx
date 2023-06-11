f6de44f5e217b17c641a96636d364deac4b346eb
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-06-11 17:54:30 -0500

    feat: add title to posts page

--- app/posts/components/posts/posts.tsx
+++ app/posts/components/posts/posts.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import { DateTime } from '@/components/date-time';
+import { Comment } from '@/components/comment';
 import { LinkCtr } from '@/components/link-ctr';
 import { Body } from '@/components/body';
 import type { Post } from '@/lib/types';
@@ -13,61 +14,70 @@ export function Posts({ posts }: { posts: Post[] }) {
 
     return (
         <>
-            {dates.map((date, dateIndex) => {
-                return (
-                    <div
-                        key={date}
-                        className={styles['posts-ctr']}
-                        data-date={date}
-                    >
-                        {dateIndex !== 0 && (
-                            <DateTime
-                                className={styles.date}
-                                dateString={date}
-                            />
-                        )}
-                        <div className={styles.posts}>
-                            {last20Posts
-                                .filter(
-                                    (post) => post.date.split('T')[0] === date
-                                )
-                                .map(
-                                    (
-                                        { slug, title, date, body },
-                                        postIndex
-                                    ) => {
-                                        return (
-                                            <article
-                                                key={slug}
-                                                className={styles.post}
-                                            >
-                                                <h2
-                                                    className={classNames(
-                                                        styles.title,
-                                                        {
-                                                            [styles.first]:
-                                                                dateIndex ===
-                                                                    0 &&
-                                                                postIndex === 0,
-                                                        }
-                                                    )}
+            <div className={styles['page-title']}>
+                <Comment text="Posts" />
+                <DateTime className={styles.date} dateString={dates[0]} />
+            </div>
+            <div className={styles['date-groups']}>
+                {dates.map((date, dateIndex) => {
+                    return (
+                        <div
+                            key={date}
+                            className={styles['posts-ctr']}
+                            data-date={date}
+                        >
+                            {dateIndex !== 0 && (
+                                <DateTime
+                                    className={styles.date}
+                                    dateString={date}
+                                />
+                            )}
+                            {/* <DateTime className={styles.date} dateString={date} /> */}
+                            <div className={styles.posts}>
+                                {last20Posts
+                                    .filter(
+                                        (post) =>
+                                            post.date.split('T')[0] === date
+                                    )
+                                    .map(
+                                        (
+                                            { slug, title, date, body },
+                                            postIndex
+                                        ) => {
+                                            return (
+                                                <article
+                                                    key={slug}
+                                                    className={styles.post}
                                                 >
-                                                    <LinkCtr
-                                                        href={`/posts/${slug}`}
+                                                    <h2
+                                                        className={classNames(
+                                                            styles.title,
+                                                            {
+                                                                [styles.first]:
+                                                                    dateIndex ===
+                                                                        0 &&
+                                                                    postIndex ===
+                                                                        0,
+                                                            }
+                                                        )}
                                                     >
-                                                        {title}
-                                                    </LinkCtr>
-                                                </h2>
+                                                        <LinkCtr
+                                                            href={`/posts/${slug}`}
+                                                        >
+                                                            {title}
+                                                        </LinkCtr>
+                                                    </h2>
 
-                                                <Body>{body}</Body>
-                                            </article>
-                                        );
-                                    }
-                                )}
+                                                    <Body>{body}</Body>
+                                                </article>
+                                            );
+                                        }
+                                    )}
+                            </div>
                         </div>
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </div>
         </>
     );
 }
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,14 +2,13 @@ import { Page } from '@/components/page';
 import { Main } from '@/components/main';
 import { Posts } from './components/posts';
 import getPosts from '@/lib/get-posts';
-import styles from './page.module.scss';
 
 export default async function PostsPage() {
     const posts = await getPosts();
 
     return (
         <Page links={[{ label: 'Home', path: '/' }]}>
-            <Main columns={2} right={{ className: styles.gap }}>
+            <Main columns={2}>
                 <Posts posts={posts} />
             </Main>
         </Page>
