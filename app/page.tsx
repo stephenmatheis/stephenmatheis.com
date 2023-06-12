fa8599306d360cedfd3ce9145fab729296359131
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-06-12 13:48:29 -0500

    feat: move memoji to separate component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Page } from '@/components/page';
 import { Main } from '@/components/main';
+import { Memoji } from '@/components/memoji';
 
 export default function RootPage() {
     return (
@@ -13,19 +14,7 @@ export default function RootPage() {
             ]}
         >
             <Main columns={2}>
-                <video
-                    autoPlay
-                    loop
-                    playsInline
-                    muted
-                    preload="auto"
-                    style={{
-                        width: 640 / 2,
-                        height: 480 / 2,
-                        margin: 'auto auto',
-                    }}
-                    src="/memoji-loop-22s.mov"
-                />
+                <Memoji />
             </Main>
         </Page>
     );
