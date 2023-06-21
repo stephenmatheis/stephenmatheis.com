f05bf8849308ea97904d41e25809223b951d1729
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-06-20 20:40:19 -0500

    debug: memoji

--- components/memoji/memoji.tsx
+++ components/memoji/memoji.tsx
@@ -1,20 +1,53 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import styles from './memoji.module.scss';
 
 export function Memoji({}: {}) {
+    const [canPlay, setCanPlay] = useState(false);
+    const [seconds, setSeconds] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+            setSeconds((prev) => prev + 1);
+        }, 1000);
+    }, []);
+
     return (
-        <video
-            autoPlay
-            loop
-            playsInline
-            muted
-            preload="auto"
-            style={{
-                width: 640 / 2,
-                height: 480 / 2,
-                margin: 'auto auto',
-            }}
-            className={styles.memoji}
-            src="/memoji-loop-22s.mov"
-        />
+        <>
+            {!canPlay && (
+                <div style={{ textAlign: 'center' }}>Loading {seconds}...</div>
+            )}
+            <video
+                onCanPlay={(event) => {
+                    console.log('onCanPlay', event);
+                    setCanPlay(true);
+                }}
+                onCanPlayThrough={(event) => {
+                    console.log('onCanPlayThrough', event);
+                }}
+                onLoadedMetadata={(event) => {
+                    console.log('onLoadedMetadata', event);
+                }}
+                onLoadStart={(event) => {
+                    console.log('onLoadStart', event);
+                }}
+                onPlay={(event) => {
+                    console.log('onPlay', event);
+                }}
+                autoPlay
+                playsInline
+                loop
+                muted
+                preload="auto"
+                style={{
+                    width: 640 / 2,
+                    height: 480 / 2,
+                    margin: 'auto auto',
+                }}
+                className={styles.memoji}
+                src="/memoji-loop-22s.mov"
+            />
+        </>
     );
 }
