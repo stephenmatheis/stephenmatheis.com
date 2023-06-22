22d127a91af522ebfb81193705aebaa1ead926c9
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-06-22 15:49:45 -0500

    feat: rss

--- scripts/rss.ts
+++ scripts/rss.ts
@@ -1,16 +1,18 @@
-import fs from 'fs';
+import { readdirSync, readFileSync, writeFileSync } from 'fs';
 import RSS from 'rss';
 import path from 'path';
 import { marked } from 'marked';
+import { gfmHeadingId } from 'marked-gfm-heading-id';
+import { mangle } from 'marked-mangle';
+
 import matter from 'gray-matter';
 
-const posts = fs
-    .readdirSync(path.resolve(__dirname, '../posts/'))
+const posts = readdirSync(path.resolve(__dirname, '../_posts/'))
     .filter(
         (file) => path.extname(file) === '.md' || path.extname(file) === '.mdx'
     )
     .map((file) => {
-        const postContent = fs.readFileSync(`./posts/${file}`, 'utf8');
+        const postContent = readFileSync(`./_posts/${file}`, 'utf8');
         const { data, content }: { data: any; content: string } =
             matter(postContent);
         return { ...data, body: content };
@@ -22,27 +24,27 @@ const renderer = new marked.Renderer();
 renderer.link = (href, _, text) =>
     `<a href="${href}" target="_blank" rel="noopener noreferrer">${text}</a>`;
 
-marked.setOptions({
+marked.use(gfmHeadingId(), mangle(), {
     gfm: true,
     breaks: true,
-    headerIds: true,
     renderer,
 });
 
-const renderPost = (md: string) => marked.parse(md);
+function renderPost(md: string) {
+    return marked.parse(md);
+}
 
-const main = () => {
+function main(): void {
     const feed = new RSS({
-        title: 'Max Leiter',
-        site_url: 'https://maxleiter.com',
-        feed_url: 'https://maxleiter.com/feed.xml',
-        // image_url: 'https://maxleiter.com/og.png',
+        title: 'Stephen Matheis',
+        site_url: 'https://stephenmatheis-com.vercel.app',
+        feed_url: 'https://stephenmatheis-com.vercel.app/feed.xml',
         language: 'en',
-        description: "Max Leiter's blog",
+        description: "Stephen Matheis' blog",
     });
 
     posts.forEach((post) => {
-        const url = `https://maxleiter.com/blog/${post.slug}`;
+        const url = `https://stephenmatheis-com.vercel.app/posts/${post.slug}`;
 
         feed.item({
             title: post.title,
@@ -55,7 +57,7 @@ const main = () => {
     });
 
     const rss = feed.xml({ indent: true });
-    fs.writeFileSync(path.join(__dirname, '../public/feed.xml'), rss);
-};
+    writeFileSync(path.join(__dirname, '../public/feed.xml'), rss);
+}
 
 main();
