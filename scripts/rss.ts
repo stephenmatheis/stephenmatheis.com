e9b9cc03292c4d8ee008f5237222d92d9f416f4f
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-06-22 13:48:03 -0500

    feat: add rss and marked packages

---
+++ scripts/rss.ts
@@ -0,0 +1,61 @@
+import fs from 'fs';
+import RSS from 'rss';
+import path from 'path';
+import { marked } from 'marked';
+import matter from 'gray-matter';
+
+const posts = fs
+    .readdirSync(path.resolve(__dirname, '../posts/'))
+    .filter(
+        (file) => path.extname(file) === '.md' || path.extname(file) === '.mdx'
+    )
+    .map((file) => {
+        const postContent = fs.readFileSync(`./posts/${file}`, 'utf8');
+        const { data, content }: { data: any; content: string } =
+            matter(postContent);
+        return { ...data, body: content };
+    })
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+const renderer = new marked.Renderer();
+
+renderer.link = (href, _, text) =>
+    `<a href="${href}" target="_blank" rel="noopener noreferrer">${text}</a>`;
+
+marked.setOptions({
+    gfm: true,
+    breaks: true,
+    headerIds: true,
+    renderer,
+});
+
+const renderPost = (md: string) => marked.parse(md);
+
+const main = () => {
+    const feed = new RSS({
+        title: 'Max Leiter',
+        site_url: 'https://maxleiter.com',
+        feed_url: 'https://maxleiter.com/feed.xml',
+        // image_url: 'https://maxleiter.com/og.png',
+        language: 'en',
+        description: "Max Leiter's blog",
+    });
+
+    posts.forEach((post) => {
+        const url = `https://maxleiter.com/blog/${post.slug}`;
+
+        feed.item({
+            title: post.title,
+            description: renderPost(post.body),
+            date: new Date(post?.date),
+            author: 'Max Leiter',
+            url,
+            guid: url,
+        });
+    });
+
+    const rss = feed.xml({ indent: true });
+    fs.writeFileSync(path.join(__dirname, '../public/feed.xml'), rss);
+};
+
+main();
