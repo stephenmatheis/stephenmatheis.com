0c0ca94f2d0d6567bcc7b2e430903bfc9ed31cff
Steve Matheis <stephen.a.matheis@gmail.com>
2023-06-23 18:42:26 -0500

    Feat/pre commit (#19)
    Add `pre-commit` and `lint-staged`. Runs a script to set the front matter property `lastUpdated` to the modified date of markdown files in `_posts` on each commit.

--- scripts/posts-last-updated.mjs
+++ scripts/posts-last-updated.mjs
@@ -5,17 +5,36 @@ import fs from 'fs/promises';
 const postsDirectory = join(process.cwd(), '_posts');
 const posts = await fs.readdir(postsDirectory);
 
+// FIXME: This runs on all posts.
+// Filter to only run on posts that have been modified.
 posts.forEach(async (file) => {
     const filePath = join(postsDirectory, file);
     const stats = await fs.stat(filePath);
     const fileMatter = matter.read(filePath);
     const { data: currentData } = fileMatter;
+    const lastModified = new Date(stats.mtime);
     const updatedData = {
         ...currentData,
-        lastModified: new Date(stats.mtime).toISOString(),
+        lastModified: lastModified.toISOString(),
     };
+
+    // console.log(currentData.title);
+    // console.log(currentData.lastModified);
+    // console.log(updatedData.lastModified);
+    // console.log(new Date().toISOString());
+
+    if (currentData.lastModified === updatedData.lastModified) {
+        // console.log('not modified, skip');
+        // console.log('---');
+
+        return;
+    }
+
+    // console.log('modified');
+    // console.log('---');
 
     fileMatter.data = updatedData;
     const updatedFileContent = matter.stringify(fileMatter);
-    fs.writeFile(filePath, updatedFileContent);
+    await fs.writeFile(filePath, updatedFileContent);
+    await fs.utimes(filePath, lastModified, lastModified);
 });
