808b0f0ad37f5fe5a531350e0530befff22b189a
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-06-22 22:09:41 -0500

    feat: hello world

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -7,7 +7,7 @@ export function Sidebar({}) {
             <LinkCtr href="/posts">Posts</LinkCtr>
             <LinkCtr href="/archive">Archive</LinkCtr>
             <LinkCtr href="/projects">Projects</LinkCtr>
-            <LinkCtr href="/resume">About</LinkCtr>
+            <LinkCtr href="/about">About</LinkCtr>
             <LinkCtr href="/settings">Settings</LinkCtr>
         </nav>
     );
