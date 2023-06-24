3f60997996714c13d203752e4dad3dbd1b1922e7
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-06-24 14:21:08 -0500

    feat: only update staged posts

---
+++ scripts/update-posts.mjs
@@ -0,0 +1,38 @@
+// Modified from: https://www.adamcollier.co.uk/posts/adding-an-updated-date-to-markdown-and-mdx-posts/
+
+import matter from 'gray-matter';
+import { writeFile } from 'fs/promises';
+import { success, mod, cyan } from '../utils/log.mjs';
+
+const [, , ...mdFilePaths] = process.argv;
+const today = new Date().toLocaleDateString('en-US', {
+    dateStyle: 'long',
+    timeZone: process.env.NEXT_PUBLIC_TIME_ZONE,
+});
+
+console.log(cyan(`Today's date is ${today}\n`));
+
+mdFilePaths.forEach(async (path) => {
+    const file = matter.read(path);
+    const { data: currentData } = file;
+    const updatedData = {
+        ...currentData,
+        lastModified: today,
+    };
+
+    console.log('Title:', currentData.title);
+    console.log('Last Upated:', currentData.lastModified);
+
+    if (currentData.lastModified === updatedData.lastModified) {
+        console.log(success(), 'Up to date\n');
+        return;
+    }
+
+    console.log(mod(), 'Modified\n');
+
+    file.data = updatedData;
+
+    const updatedFileContent = matter.stringify(file);
+
+    writeFile(path, updatedFileContent);
+});
