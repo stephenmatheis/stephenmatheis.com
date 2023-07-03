70099afc6b9791af62954f1bc23b8f1978519e77
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-07-02 23:11:31 -0500

    feat: animate modal

--- components/version-modal/version-modal.tsx
+++ components/version-modal/version-modal.tsx
@@ -1,10 +1,21 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './verison-modal.module.scss';
+import { LinkCtr } from '../link-ctr';
 
 export function VersionModal() {
     const [show, setShow] = useState(false);
+    const modal = useRef<HTMLDivElement>(null);
+
+    function close() {
+        modal?.current?.classList.add(styles.close);
+
+        setTimeout(() => {
+            document.body.classList.remove('modal-open');
+            setShow(false);
+        }, 300);
+    }
 
     useEffect(() => {
         const shouldShow = localStorage.getItem('showVersionDialog');
@@ -18,22 +29,22 @@ export function VersionModal() {
     return (
         <>
             {show && (
-                <div className={styles['version-modal']} data-modal>
-                    <div
-                        className={styles.close}
-                        onClick={() => {
-                            document.body.classList.remove('modal-open');
-                            setShow(false);
-                        }}
-                    >
+                <div className={styles['version-modal']} ref={modal} data-modal>
+                    <div className={styles.close} onClick={close}>
                         ✕
                     </div>
                     <div className={styles.content}>
                         <h2>New settings are available.</h2>
                         <p>
-                            Changes to the default configuation have been reset.
-                            I'm sorry for the inconveneince. Hopefully you like
-                            the new options.
+                            If you made changes to the default configuation, I'm
+                            afraid they had to be reset.
+                        </p>
+                        <p>
+                            Hopefully you like the{' '}
+                            <LinkCtr href="/settings" onClick={close}>
+                                new options
+                            </LinkCtr>
+                            .
                         </p>
                     </div>
                 </div>
