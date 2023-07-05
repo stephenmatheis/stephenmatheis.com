a086da1d9d1ba3b614aff9402487be7d33781ccd
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-07-05 12:05:11 -0500

    fix: archive search wrap on mobile

--- components/comment/comment.tsx
+++ components/comment/comment.tsx
@@ -1,10 +1,10 @@
 import styles from './comment.module.scss';
 
-export function Comment({ text }) {
+export function Comment({ text, ...props }) {
     const content = `// ${text}`;
 
     return (
-        <div className={styles['comment']}>
+        <div className={styles['comment']} {...props}>
             <span>{content}</span>
         </div>
     );
