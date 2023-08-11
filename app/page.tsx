8ac8d3c9189c7809ac6508c8a6333ba888ea075b
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-08-11 03:35:55 -0500

    feat: add console menu to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Page } from '@/components/page';
 import { Main } from '@/components/main';
 import { Memoji } from '@/components/memoji';
+import { Console } from '@/components/console';
 
 export default function RootPage() {
     return (
         <Page
+            noHeader
+            noFooter
             links={[
                 {
                     label: 'Settings',
@@ -12,9 +15,7 @@ export default function RootPage() {
                 },
             ]}
         >
-            <Main columns={2}>
-                {/* <Memoji /> */}
-            </Main>
+            <Console />
         </Page>
     );
 }
