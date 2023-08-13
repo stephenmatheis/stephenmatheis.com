ee4ec1b37150cdf95406bacf8ea60a68d277cb23
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-08-13 14:22:35 -0500

    feat: add prompts for jobs, skills, & contacts

--- components/skills/skills.tsx
+++ components/skills/skills.tsx
@@ -1,3 +1,4 @@
+import { UpdatePrompts } from '@/components/update-prompts';
 import { Indicator } from '@/components/indicator';
 import { Comment } from '@/components/comment';
 import skills from '@/data/skills';
@@ -6,13 +7,30 @@ import styles from './skills.module.scss';
 export function Skills() {
     return (
         <div className={styles['skills']}>
+            <UpdatePrompts
+                prompts={[
+                    {
+                        label: 'Skills',
+                        type: 'content',
+                    },
+                    ...skills.map(({ group }) => {
+                        return {
+                            label: group,
+                            type: 'job',
+                        };
+                    }),
+                ]}
+            />
             <Indicator label={'Skills'} />
             <Comment text={'Skills'} />
             <div className={styles['groups-ctr']}>
                 {skills.map(({ group, items }, index) => {
                     return (
                         <div key={index} className={styles['group']}>
-                            <div className={styles['name']}>{group}</div>
+                            <div className={styles['name']}>
+                                <Indicator label={group} />
+                                {group}
+                            </div>
                             <span className={styles['items-ctr']}>
                                 {items.map((item, index) => {
                                     return (
