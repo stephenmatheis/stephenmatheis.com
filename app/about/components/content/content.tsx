cda72ebee14730590c5aa4572045f78958e0d363
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-08-13 14:22:35 -0500

    feat: add prompts for jobs, skills, & contacts

--- app/about/components/content/content.tsx
+++ app/about/components/content/content.tsx
@@ -1,37 +1,12 @@
-'use client';
-
 import { Experience } from '@/components/experience';
 import { Skills } from '@/components/skills';
 import { Projects } from '@/components/projects';
 import { Contact } from '@/components/contact';
-import { UpdatePrompts } from '@/components/update-prompts';
 import styles from './content.module.scss';
-
-// TODO: Add to prompts
 
 export function Content() {
     return (
         <div className={styles.resume}>
-            <UpdatePrompts
-                // TODO: Move to Experience, Skils, and Contact
-                // Add a prompt for each job, skill block, and contact?
-                // At least one for each contact type so enter works
-                // to launch a new email or link
-                prompts={[
-                    {
-                        label: 'Experience',
-                        type: 'content',
-                    },
-                    {
-                        label: 'Skills',
-                        type: 'content',
-                    },
-                    {
-                        label: 'Contact',
-                        type: 'content',
-                    },
-                ]}
-            />
             <section className={styles.left}>
                 <Experience />
             </section>
--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,20 +1,39 @@
-import { LinkCtr } from '@/components/link-ctr';
-import { Comment } from '@/components/comment';
+import { UpdatePrompts } from '@/components/update-prompts';
 import { Indicator } from '@/components/indicator';
+import { Comment } from '@/components/comment';
+import { LinkCtr } from '@/components/link-ctr';
 import contact from '@/data/contact';
 import styles from './contact.module.scss';
 
 export function Contact() {
     return (
         <div className={styles['contact']}>
+            <UpdatePrompts
+                prompts={[
+                    {
+                        label: 'Contact',
+                        type: 'content',
+                    },
+                    ...contact.map(({ label, href }) => {
+                        return {
+                            label: label,
+                            path: href,
+                            type: 'contact',
+                        };
+                    }),
+                ]}
+            />
             <Indicator label={'Contact'} />
             <Comment text={'Contact'} />
             <div className={styles['info']}>
-                {contact.map(({ href, text, label }) => {
+                {contact.map(({ text, href, label }) => {
                     return (
-                        <LinkCtr key={text} href={href} label={label}>
-                            {text}
-                        </LinkCtr>
+                        <div key={text}>
+                            <Indicator label={label} />
+                            <LinkCtr href={href} label={label}>
+                                {text}
+                            </LinkCtr>
+                        </div>
                     );
                 })}
             </div>
