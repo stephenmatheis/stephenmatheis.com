fdf82386e44dfd905aedafb8a698db4ab44491a9
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-08-14 02:56:32 -0500

    feat: add d-pad arrows, angle menu buttons

--- components/d-pad/d-pad.tsx
+++ components/d-pad/d-pad.tsx
@@ -18,7 +18,16 @@ export function DPad() {
                             new KeyboardEvent('keydown', { key: 'ArrowUp' })
                         );
                     }}
-                />
+                >
+                    <svg
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+                    </svg>
+                </div>
                 <div className={styles.middle}>
                     <div
                         className={styles.left}
@@ -31,7 +40,16 @@ export function DPad() {
                                 })
                             );
                         }}
-                    />
+                    >
+                        <svg
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            viewBox="0 0 16 16"
+                        >
+                            <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
+                        </svg>
+                    </div>
                     <div
                         className={styles.right}
                         onClick={() => {
@@ -43,7 +61,16 @@ export function DPad() {
                                 })
                             );
                         }}
-                    />
+                    >
+                        <svg
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            viewBox="0 0 16 16"
+                        >
+                            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                        </svg>
+                    </div>
                 </div>
                 <div
                     className={styles.down}
@@ -54,7 +81,16 @@ export function DPad() {
                             new KeyboardEvent('keydown', { key: 'ArrowDown' })
                         );
                     }}
-                />
+                >
+                    <svg
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                    </svg>
+                </div>
             </div>
             <div className={styles.menu}>
                 <button className={styles['btn-ctr']}>
