9405c62cc3b39376035b553d548eaa04d72222ab
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-09-03 21:48:42 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default async function RootPage() {
             ]}
         >
             <Main>
-                <Posts posts={posts} />
+                {/* <Posts posts={posts} /> */}
             </Main>
         </Page>
     );
