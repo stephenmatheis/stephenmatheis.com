2c6191db09fb11a13a60a6cc99bea03d6f0f0547
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-09-18 17:13:34 -0500

    fix: tap target size

--- components/post-title/post-title.tsx
+++ components/post-title/post-title.tsx
@@ -43,7 +43,7 @@ export function PostTitle({
                             status === 'draft'
                                 ? 'var(--muted)'
                                 : 'var(--primary)',
-                        marginLeft: '2ch',
+                        marginLeft: '1ch',
                     }}
                 >
                     ({status})
