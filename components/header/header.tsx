32f88f07ee85cde0ae8dfd7643b6a02078739ae5
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-11-25 13:16:40 -0600

    feat: add GH link to header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,12 @@
 import Link from 'next/link';
+import { LinkCtr } from '@/components/link-ctr';
+import contact from '@/data/contact';
 import styles from './header.module.scss';
 
 export function Header({ printOnly = false }) {
+    const { text, href, label }: { text: string; href: string; label: string } =
+        contact.find(({ header }) => header)!;
+
     return (
         <>
             <header
@@ -11,7 +16,11 @@ export function Header({ printOnly = false }) {
                 ].join(' ')}
                 data-header
             >
-                <Link href="/" aria-label="Stephen Matheis Front-end Software Engineer">
+                <Link
+                    className={styles.name}
+                    href="/"
+                    aria-label="Stephen Matheis Front-end Software Engineer"
+                >
                     <span className={styles.text} data-link-text-ctr>
                         <span
                             className={[styles.name, styles.part].join(' ')}
@@ -28,6 +37,9 @@ export function Header({ printOnly = false }) {
                         </span>
                     </span>
                 </Link>
+                <LinkCtr className={styles.number} href={href} label={label}>
+                    {text}
+                </LinkCtr>
             </header>
         </>
     );
