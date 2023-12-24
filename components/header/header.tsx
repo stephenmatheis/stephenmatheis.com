677bebd117046346fd26df616f0ef360c0ca3484
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-12-24 14:08:58 -0600

    feat: sticky header on mobile (WIP)

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -14,66 +14,69 @@ export function Header({ printOnly = false }) {
     }, []);
 
     return (
-        <>
-            <header
-                className={[
-                    styles.header,
-                    ...(printOnly ? [styles['print-only']] : []),
-                ].join(' ')}
-                data-header
+        <header
+            className={[
+                styles.header,
+                ...(printOnly ? [styles['print-only']] : []),
+            ].join(' ')}
+            data-header
+        >
+            {/* Name */}
+            <Link
+                className={styles.name}
+                href="/"
+                aria-label="Stephen Matheis Front-end Software Engineer"
             >
-                <Link
-                    className={styles.name}
-                    href="/"
-                    aria-label="Stephen Matheis Front-end Software Engineer"
-                >
-                    <span className={styles.text} data-link-text-ctr>
-                        <span
-                            className={[styles.name, styles.part].join(' ')}
-                            data-link-text
-                        >
-                            {/* Stephen Matheis * * * * * * */}
-                            {/* Stephen Matheis / / / / / / */}
-                            {/* Stephen Matheis ----------- */}
-                            Stephen Matheis
-                        </span>{' '}
-                        <br data-header-break />
-                        <span
-                            className={[styles.title, styles.part].join(' ')}
-                            data-link-text
-                        >
-                            Front-end Software Engineer
-                        </span>
+                <span className={styles.text} data-link-text-ctr>
+                    <span
+                        className={[styles.name, styles.part].join(' ')}
+                        data-link-text
+                    >
+                        {/* Stephen Matheis * * * * * * */}
+                        {/* Stephen Matheis / / / / / / */}
+                        {/* Stephen Matheis ----------- */}
+                        Stephen Matheis
+                    </span>{' '}
+                    <br data-header-break />
+                    <span
+                        className={[styles.title, styles.part].join(' ')}
+                        data-link-text
+                    >
+                        Front-end Software Engineer
                     </span>
-                </Link>
-                <Link className={styles.number} href={href} aria-label={label}>
-                    {text}
-                </Link>
-                <div className={styles.links}>
-                    {[
-                        { label: 'Experience', path: '#experience' },
-                        { label: 'Skills', path: '#skills' },
-                        { label: 'Projects', path: '#projects' },
-                        { label: 'Contact', path: '#contact' },
-                        {
-                            label: 'Download',
-                            path: '/resume.pdf',
-                            newTab: true,
-                        },
-                    ].map(({ label, path, newTab }) => {
-                        return (
-                            <Link
-                                key={label}
-                                href={path}
-                                aria-label={label}
-                                {...(newTab ? { target: '_blank' } : {})}
-                            >
-                                {label}
-                            </Link>
-                        );
-                    })}
-                </div>
-            </header>
-        </>
+                </span>
+            </Link>
+            {/* Right aligned text */}
+            <Link className={styles.right} href={href} aria-label={label}>
+                {text}
+            </Link>
+            {/* Menu */}
+            <div className={styles.menu}>MENU</div>
+            {/* Links */}
+            <div className={styles.links}>
+                {[
+                    { label: 'Experience', path: '#experience' },
+                    { label: 'Skills', path: '#skills' },
+                    { label: 'Projects', path: '#projects' },
+                    { label: 'Contact', path: '#contact' },
+                    {
+                        label: 'Download',
+                        path: '/resume.pdf',
+                        newTab: true,
+                    },
+                ].map(({ label, path, newTab }) => {
+                    return (
+                        <Link
+                            key={label}
+                            href={path}
+                            aria-label={label}
+                            {...(newTab ? { target: '_blank' } : {})}
+                        >
+                            {label}
+                        </Link>
+                    );
+                })}
+            </div>
+        </header>
     );
 }
