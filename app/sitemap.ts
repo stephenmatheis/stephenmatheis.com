706115e53ad9d3a0668fa3841b768700a267442d
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-12-27 13:25:28 -0600

    feat: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,18 +3,38 @@ import { getPosts } from '@/lib/get-posts';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const posts = await getPosts();
-    const blogs = posts.map((post: any) => ({
-        url: `https://stephenmatheis/posts/${post.slug}`,
-        lastModified: post.lastModified
-            ? new Date(post.lastModified).toISOString().split('T')[0]
-            : '',
+    const blogs = posts.flatMap((post: any) => [
+        {
+            url: `https://stephenmatheis/posts/${post.slug}`,
+            lastModified: post.lastModified
+                ? new Date(post.lastModified).toISOString().split('T')[0]
+                : '',
+        },
+        {
+            url: `https://stephenmatheis/posts/${post.slug}/json`,
+            lastModified: post.lastModified
+                ? new Date(post.lastModified).toISOString().split('T')[0]
+                : '',
+        },
+        {
+            url: `https://stephenmatheis/posts/${post.slug}/markdown`,
+            lastModified: post.lastModified
+                ? new Date(post.lastModified).toISOString().split('T')[0]
+                : '',
+        },
+    ]);
+    const routes = [
+        '',
+        '/about',
+        '/blog',
+        '/posts',
+        '/posts/rss',
+        '/posts/markdown',
+        '/posts/json',
+    ].map((route) => ({
+        url: `https://stephenmatheis.com${route}`,
+        lastModified: new Date().toISOString().split('T')[0],
     }));
-    const routes = ['', '/blog', '/posts', '/rss', '/markdown', '/json'].map(
-        (route) => ({
-            url: `https://stephenmatheis.com${route}`,
-            lastModified: new Date().toISOString().split('T')[0],
-        })
-    );
 
     return [...routes, ...blogs];
 }
