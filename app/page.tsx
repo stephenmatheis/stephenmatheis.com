dc05e7adbaa63ac9d8647c1e35189ddeef89b62f
Stephen Matheis <stephen.a.matheis@gmail.com>
2023-12-30 23:42:06 -0600

    feat: update root route

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,29 @@
 import type { Metadata } from 'next';
 import { Page } from '@/components/page';
-import { Content } from '@/components/content';
 import { Main } from '@/components/main';
+import { Hero } from '@/components/hero';
 
 export const metadata: Metadata = {
     title: 'Stephen Matheis',
     description: `The personal website and blog of Stephen Matheis. Copyright (C) ${new Date().getFullYear()} Stephen Matheis.`,
 };
 
-export default function ResumePage() {
+export default function RootPage() {
     return (
         <Page
-            anchors={[
-                { label: 'Projects', path: '/#projects' },
-                { label: 'Contact', path: '/#contact' },
-                {
-                    label: 'Resume',
-                    path: '/resume.pdf',
-                    newTab: true,
-                },
-            ]}
-            links={[
-                {
-                    label: 'Resume',
-                    path: '/resume.pdf',
-                    newTab: true,
-                },
-            ]}
+            anchors={[{ label: 'RSS', path: '/rss', newTab: true }]}
+            links={
+                [
+                    // {
+                    //     label: '',
+                    //     path: '',
+                    // },
+                ]
+            }
+            noFooter
         >
             <Main>
-                <Content />
+                <Hero />
             </Main>
         </Page>
     );
