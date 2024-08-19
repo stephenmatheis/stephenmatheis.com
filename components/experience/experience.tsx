e7b51764b68bfdf4db59a0a9ce0a558039ea0826
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-08-18 22:08:27 -0400

    feat: dot (WIP)

--- components/experience/experience.tsx
+++ components/experience/experience.tsx
@@ -6,46 +6,147 @@ export function Experience() {
     return (
         <Section className={styles.experience} heading="Experience">
             {jobs.map(
-                ({ title, company, location, start, end, lines }, index) => {
+                (
+                    { title, company, location, start, end, lines, titles },
+                    index
+                ) => {
                     return (
                         <div key={index} className={styles['job']}>
-                            <div className={styles['title']}>
-                                <span>{title}</span>{' '}
-                                <span className={styles['company']}>
-                                    @ {company}
-                                </span>
-                            </div>
-                            <div className={styles['date']}>
-                                {start} - {end}
-                                <span className={styles['location']}>
-                                    {' '}
-                                    • {location}
-                                </span>
-                            </div>
-                            <div className={styles['lines']}>
-                                {lines.map((line, index) => {
-                                    return (
-                                        <span
-                                            key={index}
-                                            className={styles['line']}
-                                        >
-                                            <span
-                                                style={{
-                                                    marginRight: '6px',
-                                                    color: 'var(--muted)',
-                                                }}
-                                            >
-                                                {'*'}
-                                            </span>
-                                            <span
-                                                dangerouslySetInnerHTML={{
-                                                    __html: line,
-                                                }}
-                                            />
+                            {lines && (
+                                <>
+                                    <div className={styles['title']}>
+                                        <span>{title}</span>{' '}
+                                        <span className={styles['company']}>
+                                            @ {company}
                                         </span>
-                                    );
-                                })}
-                            </div>
+                                    </div>
+                                    <div className={styles['date']}>
+                                        {start} - {end}
+                                        <span className={styles['location']}>
+                                            {' '}
+                                            • {location}
+                                        </span>
+                                    </div>
+                                    <div className={styles['lines']}>
+                                        {lines.map((line, index) => {
+                                            return (
+                                                <span
+                                                    key={index}
+                                                    className={styles['line']}
+                                                >
+                                                    <span
+                                                        style={{
+                                                            marginRight: '6px',
+                                                            color: 'var(--muted)',
+                                                        }}
+                                                    >
+                                                        {'*'}
+                                                    </span>
+                                                    <span
+                                                        dangerouslySetInnerHTML={{
+                                                            __html: line,
+                                                        }}
+                                                    />
+                                                </span>
+                                            );
+                                        })}
+                                    </div>
+                                </>
+                            )}
+                            {titles && (
+                                <>
+                                    <div className={styles['title']}>
+                                        <span>{company}</span>
+                                    </div>
+                                    <div className={styles.positions}>
+                                        {titles?.map(
+                                            ({
+                                                title,
+                                                location,
+                                                start,
+                                                end,
+                                                lines,
+                                            }) => {
+                                                return (
+                                                    <div
+                                                        key={title}
+                                                        className={
+                                                            styles.position
+                                                        }
+                                                    >
+                                                        <div
+                                                            className={
+                                                                styles['title']
+                                                            }
+                                                        >
+                                                            <span>{title}</span>
+                                                        </div>
+                                                        <div
+                                                            className={
+                                                                styles['date']
+                                                            }
+                                                        >
+                                                            {start} - {end}
+                                                            <span
+                                                                className={
+                                                                    styles[
+                                                                        'location'
+                                                                    ]
+                                                                }
+                                                            >
+                                                                {' '}
+                                                                • {location}
+                                                            </span>
+                                                        </div>
+                                                        <div
+                                                            className={
+                                                                styles['lines']
+                                                            }
+                                                        >
+                                                            {lines.map(
+                                                                (
+                                                                    line,
+                                                                    index
+                                                                ) => {
+                                                                    return (
+                                                                        <span
+                                                                            key={
+                                                                                index
+                                                                            }
+                                                                            className={
+                                                                                styles[
+                                                                                    'line'
+                                                                                ]
+                                                                            }
+                                                                        >
+                                                                            <span
+                                                                                style={{
+                                                                                    marginRight:
+                                                                                        '6px',
+                                                                                    color: 'var(--muted)',
+                                                                                }}
+                                                                            >
+                                                                                {
+                                                                                    '*'
+                                                                                }
+                                                                            </span>
+                                                                            <span
+                                                                                dangerouslySetInnerHTML={{
+                                                                                    __html: line,
+                                                                                }}
+                                                                            />
+                                                                        </span>
+                                                                    );
+                                                                }
+                                                            )}
+                                                        </div>
+                                                    </div>
+                                                );
+                                            }
+                                        )}
+                                    </div>
+                                </>
+                            )}
                         </div>
                     );
                 }
