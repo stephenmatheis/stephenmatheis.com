8f04ae133a9f35e143f854c05d705c92460b10a2
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-08-25 15:09:20 -0400

    feat: shrink mobile download button

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -9,10 +9,11 @@ export function Footer() {
                     height="16"
                     fill="currentColor"
                     viewBox="0 0 16 16"
-                    className={styles.mobile}
                 >
-                    <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z" />
-                    <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
+                    <path
+                        fillRule="evenodd"
+                        d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"
+                    />
                 </svg>
                 <span className={styles.desktop}>Download Resume</span>
             </a>
