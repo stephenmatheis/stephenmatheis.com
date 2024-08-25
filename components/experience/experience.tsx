ffc98ebedfc53e5a7d0776985e08fb46d37a3e05
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-08-25 15:05:52 -0400

    feat: update jobs, skills, and projects

--- components/experience/experience.tsx
+++ components/experience/experience.tsx
@@ -5,51 +5,58 @@ import styles from './experience.module.scss';
 export function Experience() {
     return (
         <Section className={styles.experience} heading="Experience">
-            {jobs.map(
-                ({ title, company, location, start, end, lines }, index) => {
-                    return (
-                        <div key={index} className={styles['job']}>
-                            <div className={styles['title']}>
-                                <span>{title}</span>{' '}
-                                <span className={styles['company']}>
-                                    @ {company}
-                                </span>
-                            </div>
-                            <div className={styles['date']}>
-                                {start} - {end}
-                                <span className={styles['location']}>
-                                    {' '}
-                                    • {location}
-                                </span>
-                            </div>
-                            <div className={styles['lines']}>
-                                {lines.map((line, index) => {
-                                    return (
-                                        <span
-                                            key={index}
-                                            className={styles['line']}
-                                        >
-                                            <span
-                                                style={{
-                                                    marginRight: '6px',
-                                                    color: 'var(--muted)',
-                                                }}
-                                            >
-                                                {'*'}
+            {jobs.map(({ company, location, roles, lines }, index) => {
+                return (
+                    <div key={index} className={styles.job}>
+                        <div className={styles.roles}>
+                            {roles.map(({ title, start, end }, index) => {
+                                return (
+                                    <div key={index} className={styles.role}>
+                                        <div className={styles.title}>
+                                            <span>{title}</span>{' '}
+                                            {index === 0 && (
+                                                <span
+                                                    className={styles.company}
+                                                >
+                                                    @ {company}
+                                                </span>
+                                            )}
+                                        </div>
+                                        <div className={styles.date}>
+                                            {start} - {end}
+                                            <span className={styles.location}>
+                                                {' '}
+                                                • {location}
                                             </span>
-                                            <span
-                                                dangerouslySetInnerHTML={{
-                                                    __html: line,
-                                                }}
-                                            />
-                                        </span>
-                                    );
-                                })}
-                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
                         </div>
-                    );
-                }
-            )}
+                        <div className={styles.lines}>
+                            {lines.map((line, index) => {
+                                return (
+                                    <span key={index} className={styles.line}>
+                                        <span
+                                            style={{
+                                                marginRight: '6px',
+                                                color: 'var(--muted)',
+                                            }}
+                                        >
+                                            {'•'}
+                                        </span>
+                                        <span
+                                            dangerouslySetInnerHTML={{
+                                                __html: line,
+                                            }}
+                                        />
+                                    </span>
+                                );
+                            })}
+                        </div>
+                    </div>
+                );
+            })}
         </Section>
     );
 }
