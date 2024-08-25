dcabb46610e782e0ff0a611ec511c63f45d94500
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-08-25 13:16:12 -0400

    chore: remove unused components

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,25 +1,10 @@
 import { MetadataRoute } from 'next';
-// import { getPosts } from '@/lib/get-posts';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    // const posts = await getPosts();
-    // const blogs = posts.map((post: any) => ({
-    //     url: `https://stephenmatheis/posts/${post.slug}`,
-    //     lastModified: post.lastModified
-    //         ? new Date(post.lastModified).toISOString().split('T')[0]
-    //         : '',
-    // }));
-    // const routes = ['', '/posts', '/about', '/rss', '/markdown', '/json'].map(
-    //     (route) => ({
-    //         url: `https://stephenmatheis.com${route}`,
-    //         lastModified: new Date().toISOString().split('T')[0],
-    //     })
-    // );
     const routes = [''].map((route) => ({
         url: `https://stephenmatheis.com${route}`,
         lastModified: new Date().toISOString().split('T')[0],
     }));
 
-    // return [...routes, ...blogs];
     return routes;
 }
