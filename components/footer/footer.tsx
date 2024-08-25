fd0dcbd16886a4bd048d25f1392fa7df2df7c532
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-08-25 14:18:43 -0400

    feat: fixed download link

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,42 +1,19 @@
-import Link from 'next/link';
 import styles from './footer.module.scss';
 
-type Link = {
-    label: string;
-    path: string;
-    newTab?: boolean;
-};
-
-type Props = {
-    links?: Link[];
-    text?: string;
-};
-
-export function Footer({ links = [], text }: Props) {
+export function Footer() {
     return (
         <footer className={styles.footer}>
-            {links?.length !== 0 && (
-                <nav>
-                    <ol>
-                        {links?.map(({ label, path, newTab }) => {
-                            return (
-                                <li key={path}>
-                                    <Link
-                                        href={path}
-                                        aria-label={label}
-                                        {...(newTab
-                                            ? { target: '_blank' }
-                                            : {})}
-                                    >
-                                        {label}
-                                    </Link>
-                                </li>
-                            );
-                        })}
-                    </ol>
-                </nav>
-            )}
-            {text && <div className={styles.text}>{text}</div>}
+            <a href="/resume.pdf" aria-label="My resume" target="_blank">
+                <svg
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                >
+                    <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z" />
+                    <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
+                </svg>
+            </a>
         </footer>
     );
 }
