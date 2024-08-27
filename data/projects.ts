0b9d02046b59bc1cd84717a64170dc0c96dc35df
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-08-26 20:39:58 -0400

    feat: new resume page

--- data/projects.ts
+++ data/projects.ts
@@ -1,20 +1,27 @@
 const projects = [
     {
-        name: 'Robi',
-        link: 'https://github.com/robijs',
-        description:
-            'A client-side single-page application framework for SharePoint with support for ES modules and local development.',
-    },
-    {
         name: 'Command Palette',
+        date: '2021',
         link: 'https://robi-command-palette.netlify.app',
+        image: '/images/command-palette.webp',
         description:
             'A React component to search for and run commands from within an app.',
     },
+
     {
         name: 'Samepage',
+        date: '2019',
         link: 'https://samepage.netlify.app/',
+        image: '/images/samepage.webp',
         description: 'A Basecamp clone built with Robi.',
+    },
+    {
+        name: 'Robi',
+        date: '2017',
+        link: 'https://github.com/robijs',
+        image: '/images/robi.webp',
+        description:
+            'A client-side single-page application framework for SharePoint, featuring support for ES modules and local development.',
     },
 ];
 
