c3e7c4d63ffc88d652654a9c3321373aa3a72bc8
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-08-26 20:39:58 -0400

    feat: new resume page

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -7,7 +7,7 @@ export function Contact() {
     return (
         <section className={styles.contact}>
             <Comment text="Contact" />
-            <div className={styles.info}>
+            {/* <div className={styles.info}>
                 {contact
                     .filter(({ header }) => !header)
                     .map(({ text, href, label, newTab }) => {
@@ -20,6 +20,22 @@ export function Contact() {
                                 >
                                     {text}
                                 </Link>
+                            </div>
+                        );
+                    })}
+            </div> */}
+            <div className={styles.list}>
+                {contact
+                    .filter(({ header }) => !header)
+                    .map((item, index) => {
+                        return (
+                            <div key={index} className={styles.item}>
+                                <div className={styles.name}>{item.name}</div>
+                                <div className={styles.text}>
+                                    <Link href={item.href}>
+                                        {item.user || item.text}
+                                    </Link>
+                                </div>
                             </div>
                         );
                     })}
