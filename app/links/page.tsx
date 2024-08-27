0512ff61c1fede3b5a65712d7987ed4e4773c9e3
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-08-26 22:49:03 -0400

    feat: links page (WIP)

---
+++ app/links/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss';
+
+export default function ResumePage() {
+    return (
+        <div className={styles.links}>
+            <h1>Links</h1>
+        </div>
+    );
+}
