d2ff2bd8043ecba1529d3718809e91970eecf562
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-10-01 18:14:48 -0400

    feat: new font

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -50,7 +50,7 @@ export default function ResumePage() {
 
             {/* About */}
             <div className={styles.about}>
-                <h2>About</h2>
+                {/* <h2>About</h2> */}
                 <p>
                     Hello there! My name is Stephen Matheis, and I'm the
                     Director of Customer Experience at <span>T and T</span>{' '}
@@ -179,18 +179,7 @@ export default function ResumePage() {
                     target="_blank"
                     className={styles.download}
                 >
-                    <svg
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        viewBox="0 0 16 16"
-                        className={styles.mobile}
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"
-                        />
-                    </svg>
+                    ↓
                 </Link>
             </div>
         </div>
