185834f84dc73ad3fbd1f8da3fcd800d8450e295
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-11-13 21:53:19 -0500

    fix: max page width

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Link from 'next/link';
 import styles from './page.module.scss';
-import { ScrollJacked } from '@/components/scrolljacked';
 import { Title } from '@/components/title';
 
 export default function HomePage() {
     return (
-        <div className={styles.home}>
+        <div className={styles.page}>
             <header>
                 <div className="maxwidth">
                     <div className={styles.heading}>
@@ -28,25 +26,9 @@ export default function HomePage() {
                                 shall be yours.
                             </div>
                         </div>
-                        {/* <menu>
-                            <Link href="/resume.pdf">↓ DOWNLOAD</Link>
-                            <Link
-                                href="https://github.com/stephenmatheis"
-                                target="_blank"
-                            >
-                                &gt; GITHUB
-                            </Link>
-                            <Link
-                                href="https://codepen.io/stephenmatheis"
-                                target="_blank"
-                            >
-                                # CODEPEN
-                            </Link>
-                        </menu> */}
                     </div>
                 </div>
             </header>
-            {/* <ScrollJacked /> */}
         </div>
     );
 }
