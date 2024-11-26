6a5b60595cb681ad93e3af12572b0b1dd727184e
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-11-26 06:51:30 -0500

    feat: code syntax highlight lib (WIP)

--- components/code/code.tsx
+++ components/code/code.tsx
@@ -2,17 +2,24 @@ import fs from 'fs';
 import path from 'path';
 import classNames from 'classnames';
 import styles from './code.module.scss';
+import SyntaxHighlighter from 'react-syntax-highlighter';
 
 export function Code() {
     const filePath = path.join(process.cwd(), './components/code/code.tsx');
     const blockOfText = fs.readFileSync(filePath, 'utf8');
 
-    console.log(blockOfText);
-
     return (
         <div className={classNames(styles.code, 'maxwidth')}>
             <div className={styles.diagram} />
-            <div className={styles.javascript}>Test</div>
+            <div className={styles.javascript}>
+                {/* <SyntaxHighlighter language="javascript" style={vs}> */}
+                <SyntaxHighlighter
+                    language="javascript"
+                    useInlineStyles={false}
+                >
+                    {blockOfText}
+                </SyntaxHighlighter>
+            </div>
         </div>
     );
 }
