3f4d3221fbd0e1b193aefd0d00c0b98ce5166aa9
Stephen Matheis <stephen.a.matheis@gmail.com>
2024-11-26 07:08:57 -0500

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const routes = ['', '/links', '/resume', '/print'].map((route) => ({
+    const routes = ['', '/print'].map((route) => ({
         url: `https://stephenmatheis.com${route}`,
         lastModified: new Date().toISOString().split('T')[0],
     }));
