119b940366c2fddad40c71c9fa5dd6bcac4bf222
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-04-15 18:30:39 -0400

    feat: home page

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,7 +3,7 @@ import styles from './header.module.scss';
 
 export function Header() {
     return (
-        <header className={[styles.header].join(' ')}>
+        <header className={styles.header}>
             <Link className={styles.name} href="/" aria-label="Home page">
                 <span className={styles.text}>
                     <span className={[styles.name, styles.part].join(' ')}>Stephen Matheis</span>{' '}
