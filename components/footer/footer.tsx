7adf6b3088cfc2ecf2e6de917e45a73ef87ca1a9
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-04-20 14:11:43 -0400

    feat: add projects back in (as work)

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,7 +4,7 @@ import styles from './footer.module.scss';
 export function Footer() {
     return (
         <div className={styles.footer}>
-            <a href="/resume.pdf" target="_blank" title="Link to download my resume as a pdf.">
+            <a href="/stephen-matheis-resume.pdf" target="_blank" title="Link to download my resume as a pdf.">
                 Download Resume
             </a>
             <ToggleTheme />
---
+++ data/projects.ts
@@ -0,0 +1,27 @@
+const projects = [
+    {
+        name: 'Command Palette',
+        date: '2021',
+        link: 'https://robi-command-palette.netlify.app',
+        image: '/images/command-palette.webp',
+        description: 'React command runner. Built with Fastify.',
+    },
+
+    {
+        name: 'Samepage',
+        date: '2019',
+        link: 'https://samepage.netlify.app/',
+        image: '/images/samepage.webp',
+
+        description: /*html*/ `A <a href="https://basecamp.com/" target="_blank">Basecamp</a> clone built with Robi.`,
+    },
+    {
+        name: 'Robi',
+        date: '2017',
+        link: 'https://github.com/robijs',
+        image: '/images/robi.webp',
+        description: 'A client-side SPA framework for SharePoint. Supports ES modules and local development.',
+    },
+];
+
+export default projects;
