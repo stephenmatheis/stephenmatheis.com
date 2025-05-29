4b8593de903d39e6c8af39e31fa030bea82ee40c
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-05-29 11:51:10 -0400

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -1,5 +1,17 @@
 const projects = [
     {
+        name: 'U.S. Tsunami Warning System',
+        date: '2017',
+        link: 'https://tsunami-gov.vercel.app/',
+        description: /*html*/ `Redesigned <a href="https://tsunami.gov/" target="_blank">tsunami.gov</a>.`,
+    },
+    {
+        name: 'Robi',
+        date: '2017',
+        link: 'https://github.com/robijs',
+        description: 'NPMless SharePoint SPA framework.',
+    },
+    {
         name: 'Bookmarks',
         date: '2023',
         link: 'https://github.com/stephenmatheis/bookmarks',
@@ -10,25 +22,6 @@ const projects = [
         date: '2021',
         link: 'https://robi-command-palette.netlify.app',
         description: 'Command runner built with Fastify.',
-    },
-    {
-        name: 'Samepage',
-        date: '2019',
-        link: 'https://samepage.netlify.app/',
-
-        description: /*html*/ `A <a href="https://basecamp.com/" target="_blank">Basecamp</a> clone built with Robi.`,
-    },
-    {
-        name: 'Robi',
-        date: '2017',
-        link: 'https://github.com/robijs',
-        description: 'NPMless SharePoint SPA framework.',
-    },
-    {
-        name: 'U.S. Tsunami Warning System',
-        date: '2017',
-        link: 'https://tsunami-gov.vercel.app/',
-        description: /*html*/ `Redesigned <a href="https://tsunami.gov/" target="_blank">tsunami.gov</a>.`,
     },
 ];
 
