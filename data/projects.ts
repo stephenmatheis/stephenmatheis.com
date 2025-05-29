793c1e14175a7b34dbe2c0fa427c8987da596fec
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-05-29 16:53:25 -0400

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -6,22 +6,22 @@ const projects = [
         description: /*html*/ `Redesigned <a href="https://tsunami.gov/" target="_blank">tsunami.gov</a>.`,
     },
     {
-        name: 'Robi',
-        date: '2017',
-        link: 'https://github.com/robijs',
-        description: 'NPMless SharePoint SPA framework.',
-    },
-    {
-        name: 'Bookmarks',
-        date: '2023',
-        link: 'https://github.com/stephenmatheis/bookmarks',
-        description: 'Like instapaper, but worse.',
+        name: 'Apple',
+        date: '2022',
+        link: 'https://www.apple.com/watch/',
+        description: /*html*/ `New apple.com/watch buy flow.`,
     },
     {
         name: 'Command Palette',
         date: '2021',
         link: 'https://robi-command-palette.netlify.app',
-        description: 'Command runner built with Fastify.',
+        description: 'React command runner.',
+    },
+    {
+        name: 'Robi',
+        date: '2017',
+        link: 'https://github.com/robijs',
+        description: 'SharePoint SPA framework.',
     },
 ];
 
