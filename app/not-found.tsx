1e12866d30d8ca81f198e745838c62383d2b9ce5
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-05-31 09:46:45 -0400

    chores

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-
 export default function NotFound() {
     return (
         <div
@@ -12,7 +10,6 @@ export default function NotFound() {
         >
             <div style={{ display: 'grid', placeItems: 'center', gap: 36 }}>
                 <h1 style={{ textAlign: 'center', margin: 0 }}>404 - Page not found</h1>
-                <Link href="/">Go back to Home</Link>
             </div>
         </div>
     );
