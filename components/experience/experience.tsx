f490dfd7ac38b0e8f30f333140edb8b5cdcd7716
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-06-01 13:46:30 -0400

    feat: update experience

--- components/experience/experience.tsx
+++ components/experience/experience.tsx
@@ -6,41 +6,43 @@ export function Experience() {
     return (
         <section className={styles.experience}>
             <Comment text="Experience" />
-            {experience.map(({ company, location, roles }, index) => {
-                return (
-                    <div key={index} className={styles.job}>
-                        <div className={styles.roles}>
-                            {roles.map(({ title, start, end, lines }, index) => {
-                                return (
-                                    <div key={index} className={styles.role}>
-                                        <div className={styles.title}>
-                                            <span>{title}</span> <span className={styles.company}>@ {company}</span>
+            <div className={styles.jobs}>
+                {experience.map(({ company, roles, list }, index) => {
+                    return (
+                        <div key={index} className={styles.job}>
+                            <div className={styles.roles}>
+                                {roles.map(({ title, start, end }, index) => {
+                                    return (
+                                        <div key={index} className={styles.role}>
+                                            <div className={styles.title}>
+                                                <span>{title}</span>{' '}
+                                                {index === 0 && <span className={styles.company}>@ {company}</span>}
+                                            </div>
+                                            <div className={styles.date}>
+                                                {start}–{end}
+                                            </div>
                                         </div>
-                                        <div className={styles.date}>
-                                            {start}–{end}
-                                            <span className={styles.location}> • {location}</span>
-                                        </div>
-                                        <div className={styles.lines}>
-                                            {lines.map((line, index) => {
-                                                return (
-                                                    <span key={index} className={styles.line}>
-                                                        <span className={styles.bullet}>*</span>
-                                                        <span
-                                                            dangerouslySetInnerHTML={{
-                                                                __html: line,
-                                                            }}
-                                                        />
-                                                    </span>
-                                                );
-                                            })}
-                                        </div>
-                                    </div>
-                                );
-                            })}
+                                    );
+                                })}
+                            </div>
+                            <ul className={styles.list}>
+                                {list?.map((item, index) => {
+                                    return (
+                                        <li key={index} className={styles.item}>
+                                            <span className={styles.bullet}>*</span>
+                                            <span
+                                                dangerouslySetInnerHTML={{
+                                                    __html: item,
+                                                }}
+                                            />
+                                        </li>
+                                    );
+                                })}
+                            </ul>
                         </div>
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </div>
         </section>
     );
 }
