d9a7bf46ea49f6b71b0caba22b3649bb0fe282b1
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-06-01 13:46:30 -0400

    feat: update experience

--- data/experience.ts
+++ data/experience.ts
@@ -1,145 +1,129 @@
 const experience = [
     {
+        company: 'Freelance',
+        companyShort: 'TATCS',
+        site: 'https://www.tatcs.com/',
+        roles: [
+            {
+                title: 'Software Engineer',
+                start: 'May 2025',
+                end: 'Now',
+            },
+        ],
+        list: ['Software engineering services'],
+    },
+    {
         company: 'T and T Consulting Services',
         companyShort: 'TATCS',
         site: 'https://www.tatcs.com/',
-        location: 'Washington, D.C.',
         roles: [
             {
                 title: 'Director, User Experience',
                 start: 'Apr 2024',
                 end: 'May 2025',
-                lines: [
-                    /*html*/ `Redesigned <a href="https://tsunami.gov/" target="_blank">tsunami.gov</a>`,
-                    'Built an LLM tool for technical writers',
-                ],
             },
             {
                 title: 'Senior Application Solutions Engineer',
                 start: 'Oct 2023',
                 end: 'March 2024',
-                lines: ['Supported NOAA', 'Built apps with TypeScript, Python, MongoDB, Vercel & AWS'],
             },
+            {
+                title: 'Senior SharePoint Developer',
+                start: 'Mar 2018',
+                end: 'May 2021',
+            },
+        ],
+        list: [
+            /*html*/ `Redesigned <a href="https://tsunami.gov/" target="_blank">tsunami.gov</a>`,
+            'Built apps with TypeScript, Python, MongoDB, Vercel & AWS',
         ],
     },
     {
         company: 'Apple (TEKsystems)',
         site: 'https://www.apple.com/',
-        location: 'Remote',
         roles: [
             {
                 title: 'Front-end Developer',
                 start: 'Aug 2022',
                 end: 'Oct 2023',
-                lines: [
-                    'Built prototypes with Next.js, TypeScript, and Sass',
-                    'Developed a tool for scaffolding React components',
-                ],
             },
+        ],
+        list: [
+            'Built prototypes with Next.js, TypeScript, and Sass',
+            'Created a tool for scaffolding React components',
         ],
     },
     {
         company: 'GovCIO',
         site: 'https://govcio.com/',
-        location: 'San Antonio, TX',
         roles: [
             {
                 title: 'Software Programmer 5',
                 start: 'May 2022',
                 end: 'Jul 2022',
-                lines: [
-                    'Application development for the U.S Air Force',
-                    'Developed tools for generating C# source code files',
-                ],
             },
         ],
+        list: ['Application development for the U.S Air Force', 'Developed tools for generating C# source code files'],
     },
     {
         company: 'Robi',
         site: 'https://github.com/robijs',
-        location: 'San Antonio, TX',
         roles: [
             {
-                title: 'Software Developer',
+                title: 'Software Engineer',
                 start: 'Apr 2022',
                 end: 'May 2022',
-                lines: ['Ported Robi to React with support for PnPjs', 'Open sourced Robi'],
             },
         ],
+        list: ['Ported Robi to React with support for PnPjs', 'Open sourced Robi'],
     },
     {
         company: 'Aeyon',
         site: 'https://aeyon.us/',
-        location: 'San Antonio, TX',
         roles: [
             {
                 title: 'Subject Matter Expert II',
                 start: 'May 2021',
                 end: 'Apr 2022',
-                lines: [
-                    'Developed a mobile app for the U.S. Marine Corps',
-                    'Developed web apps with Robi for SharePoint',
-                ],
             },
         ],
-    },
-    {
-        company: 'T and T Consulting Services',
-        site: 'https://www.tatcs.com/',
-        location: 'San Antonio, TX',
-        roles: [
-            {
-                title: 'Senior SharePoint Developer',
-                start: 'Mar 2018',
-                end: 'May 2021',
-                lines: ['Developed web apps with Robi for SharePoint', 'Team lead'],
-            },
-        ],
+        list: ['Built a mobile app for the U.S. Marine Corps', 'Developed web apps with Robi for SharePoint'],
     },
     {
         company: 'Robi',
         site: 'https://github.com/robijs',
-        location: 'Washington, D.C.',
         roles: [
             {
-                title: 'Software Developer',
+                title: 'Software Engineer',
                 start: 'Oct 2017',
                 end: 'Mar 2018',
-                lines: ['Created Robi, a client-side SharePoint app framework', 'Freelance work'],
             },
         ],
+        list: ['Created Robi, a client-side SharePoint app framework', 'Freelance work'],
     },
     {
         company: 'HID Global',
         site: 'https://www.hidglobal.com/',
-        location: 'Austin, TX',
         roles: [
             {
                 title: 'Systems Programmer 3',
                 start: 'Sep 2016',
                 end: 'Oct 2017',
-                lines: ['Developed web apps for SharePoint', 'SharePoint farm migration'],
             },
         ],
+        list: ['Built apps for SharePoint', 'SharePoint farm migration'],
     },
     {
         company: 'Bravo Consulting Group',
         site: 'https://bravocg.com/',
-        location: 'Reston, VA',
         roles: [
             {
                 title: 'Senior Consultant',
-                start: 'Feb 2015',
-                end: 'May 2016',
-                lines: ['Developed dashboards with SharePoint', 'Team lead'],
-            },
-            {
-                title: 'Consultant',
                 start: 'Nov 2014',
-                end: 'Feb 2015',
-                lines: ['Developed apps for SharePoint', 'Automated administration with PowerShell'],
+                end: 'May 2016',
             },
         ],
+        list: ['Built apps for SharePoint', 'Team lead'],
     },
 ];
 
