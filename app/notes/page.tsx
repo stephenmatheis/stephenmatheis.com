d40a784cda90195167fe84094ab83e0c1c3002b6
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-06-23 18:08:19 -0400

    feat: loading

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -71,6 +71,7 @@ function getActionLabel(currentPos: string, direction: actions): string {
 }
 
 export default function UIPage() {
+    const [loading, setLoading] = useState<boolean>(true);
     const [showMenu, setShowMenu] = useState<boolean>(true);
     const [action, setAction] = useState<actions>('center');
     const [pos, setPos] = useState<string>('0,0');
@@ -85,6 +86,7 @@ export default function UIPage() {
             const parsedNotes: Note[] = JSON.parse(savedNotes);
 
             setNotes(parsedNotes);
+            setLoading(false);
         } else {
             console.log('No notes found in localStorage, starting with an empty array.');
         }
@@ -103,130 +105,144 @@ export default function UIPage() {
 
     return (
         <>
-            <motion.div
-                ref={pageRef}
-                className={styles.page}
-                variants={variants}
-                initial="center"
-                animate={action}
-                transition={transition}
-            >
-                {/* Note field */}
-                <textarea
-                    ref={noteRef}
-                    className={styles.note}
-                    autoFocus
-                    autoCorrect="off"
-                    spellCheck="false"
-                    // placeholder="new note"
-                    value={notes.find((note) => note.id === pos)?.content || ''}
-                    onChange={(event) => {
-                        setNotes((prev) => {
-                            const note = prev.find((note) => note.id === pos);
+            {loading ? (
+                <></>
+            ) : (
+                <>
+                    <motion.div
+                        ref={pageRef}
+                        className={styles.page}
+                        variants={variants}
+                        initial="center"
+                        animate={action}
+                        transition={transition}
+                    >
+                        {/* Note field */}
+                        <textarea
+                            ref={noteRef}
+                            className={styles.note}
+                            autoFocus
+                            autoCorrect="off"
+                            spellCheck="false"
+                            // placeholder="new note"
+                            value={notes.find((note) => note.id === pos)?.content || ''}
+                            onChange={(event) => {
+                                setNotes((prev) => {
+                                    const note = prev.find((note) => note.id === pos);
 
-                            console.log('Note changed:', event.target.value);
+                                    console.log('Note changed:', event.target.value);
 
-                            if (!note) {
-                                console.log('Adding new note at position:', pos);
-                                return [...prev, { id: pos, content: event.target.value }];
-                            }
+                                    if (!note) {
+                                        console.log('Adding new note at position:', pos);
+                                        return [...prev, { id: pos, content: event.target.value }];
+                                    }
 
-                            console.log('Updating note at position:', pos);
+                                    console.log('Updating note at position:', pos);
 
-                            return prev.map((note) =>
-                                note.id === pos ? { ...note, content: event.target.value } : note
-                            );
-                        });
-                    }}
-                />
-            </motion.div>
+                                    return prev.map((note) =>
+                                        note.id === pos ? { ...note, content: event.target.value } : note
+                                    );
+                                });
+                            }}
+                        />
+                    </motion.div>
 
-            {/* Animate to next note */}
-            {action !== 'center' && (
-                <motion.div
-                    ref={pageRef}
-                    className={`${styles.page} ${styles.target}`}
-                    variants={variants}
-                    initial={getOppositeDirection(action)}
-                    animate="center"
-                    transition={transition}
-                    onAnimationComplete={() => {
-                        setPos((prev) => getNextPosition(prev, action));
-                        setAction('reset');
-                    }}
-                >
-                    <textarea
-                        className={styles.note}
-                        autoFocus
-                        autoCorrect="off"
-                        spellCheck="false"
-                        readOnly
-                        // TODO:
-                        defaultValue={notes.find((note) => note.id === getNextPosition(pos, action))?.content || ''}
-                    />
-                </motion.div>
-            )}
+                    {/* Animate to next note */}
+                    {action !== 'center' && (
+                        <motion.div
+                            ref={pageRef}
+                            className={`${styles.page} ${styles.target}`}
+                            variants={variants}
+                            initial={getOppositeDirection(action)}
+                            animate="center"
+                            transition={transition}
+                            onAnimationComplete={() => {
+                                setPos((prev) => getNextPosition(prev, action));
+                                setAction('reset');
+                            }}
+                        >
+                            <textarea
+                                className={styles.note}
+                                autoFocus
+                                autoCorrect="off"
+                                spellCheck="false"
+                                readOnly
+                                // TODO:
+                                defaultValue={
+                                    notes.find((note) => note.id === getNextPosition(pos, action))?.content || ''
+                                }
+                            />
+                        </motion.div>
+                    )}
 
-            {/* Menu bar */}
-            <div className={styles.menubar}>
-                <div className={styles.left}>
-                    <button className={styles.togglemenu} onClick={() => setShowMenu((prev) => !prev)}>
-                        {showMenu ? 'O┄' : '┈0'}
-                    </button>
-                </div>
+                    {/* Menu bar */}
+                    <div className={styles.menubar}>
+                        <div className={styles.left}>
+                            <button className={styles.togglemenu} onClick={() => setShowMenu((prev) => !prev)}>
+                                {showMenu ? 'O┄' : '┈0'}
+                            </button>
+                        </div>
 
-                <div className={styles.right}>❱❯❭</div>
-            </div>
+                        <div className={styles.right}>
+                            <button className={styles.menu}>
+                                <span>❱</span>
+                                <span>❯</span>
+                                <span>❭</span>
+                            </button>
+                        </div>
+                    </div>
 
-            {/* Status bar */}
-            <motion.div
-                variants={{
-                    show: { opacity: 1, y: 0 },
-                    hide: { opacity: 0, y: '100%' },
-                }}
-                initial="show"
-                animate={showMenu ? 'show' : 'hide'}
-                transition={{ type: 'spring', stiffness: 300, damping: 20 }}
-                className={styles.statusbar}
-            >
-                <div className={styles.left}>
-                    <button className={styles.crossbar}>✧</button>
-                </div>
-
-                <div className={styles.right}>
-                    <span>Position: {pos}</span> <span>Notes: {notes.length}</span>
-                </div>
-            </motion.div>
-
-            {/* Actions */}
-            {[
-                { direction: 'up', label: '↑', hide: { opacity: 0, y: '-100%' } },
-                { direction: 'down', label: '↓', hide: { opacity: 0, y: '100%' } },
-                { direction: 'right', label: '→', hide: { opacity: 0, x: '100%' } },
-                { direction: 'left', label: '←', hide: { opacity: 0, x: '-100%' } },
-            ].map(({ direction, label, hide }) => {
-                const nextPos = getActionLabel(pos, direction as actions);
-                const existingNote = notes.find((note) => note.id === nextPos);
-
-                return (
-                    <motion.button
-                        key={direction}
-                        className={`${styles.arrow} ${styles[direction]}`}
-                        onClick={() => {
-                            setAction(direction as actions);
-                        }}
+                    {/* Status bar */}
+                    <motion.div
                         variants={{
                             show: { opacity: 1, y: 0 },
-                            hide,
+                            hide: { opacity: 0, y: '100%' },
                         }}
                         initial="show"
                         animate={showMenu ? 'show' : 'hide'}
                         transition={{ type: 'spring', stiffness: 300, damping: 20 }}
+                        className={styles.statusbar}
                     >
-                        {existingNote ? label : '+'}
-                    </motion.button>
-                );
-            })}
+                        <div className={styles.left}>
+                            <button className={styles.crossbar}>✧</button>
+                        </div>
+
+                        <div className={styles.right}>
+                            <span>Position: {pos}</span> <span>Notes: {notes.length}</span>
+                        </div>
+                    </motion.div>
+
+                    {/* Actions */}
+                    {[
+                        { direction: 'up', label: '△', hide: { opacity: 0, y: '-100%' } },
+                        { direction: 'down', label: '▽', hide: { opacity: 0, y: '100%' } },
+                        { direction: 'right', label: '▷', hide: { opacity: 0, x: '100%' } },
+                        { direction: 'left', label: '◁', hide: { opacity: 0, x: '-100%' } },
+                    ].map(({ direction, label, hide }) => {
+                        const nextPos = getNextPosition(getNextPosition(pos, action as actions), direction as actions);
+                        const existingNote = notes.find((note) => note.id === nextPos);
+
+                        return (
+                            <motion.button
+                                key={direction}
+                                className={`${styles.arrow} ${styles[direction]}`}
+                                onClick={() => {
+                                    setAction(direction as actions);
+                                }}
+                                variants={{
+                                    show: { opacity: 1, y: 0 },
+                                    hide,
+                                }}
+                                initial="show"
+                                animate={showMenu ? 'show' : 'hide'}
+                                transition={{ type: 'spring', stiffness: 300, damping: 20 }}
+                            >
+                                {existingNote ? label : '+'}
+                            </motion.button>
+                        );
+                    })}
+                </>
+            )}
         </>
     );
 }
