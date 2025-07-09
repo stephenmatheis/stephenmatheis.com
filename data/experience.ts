c5d53a4aa6846a440b916699f7ba67516363ee9f
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-07-09 15:42:27 -0400

    Update experience.ts

--- data/experience.ts
+++ data/experience.ts
@@ -41,7 +41,7 @@ const experience = [
                 with Next.js, TypeScript, Python, MongoDB, Vercel & AWS. 
             `,
             'Led our cross-functional UI/UX design and engineering teams.',
-            'Taught engineers and aspiring programmers of all skill levels how to build modern web applications.',
+            'Taught engineers and aspiring programmers of all skill levels how to design, build, host, and support modern web applications.',
             'Wrote technical portions for new proposals.',
         ],
     },
@@ -56,8 +56,8 @@ const experience = [
             },
         ],
         list: [
-            'Worked with Apple’s design and production engineering teams to create prototypes for new products and services  with Next.js, TypeScript, Sass, Jest, and Storybook',
-            'Participated in code reviews and provided feedback to other developers',
+            'Worked with Apple’s design and production engineering teams to create prototypes for new products and services  with Next.js, TypeScript, Sass, Jest, and Storybook.',
+            'Participated in code reviews and provided feedback to other developers.',
             "Created a tool for scaffolding React components. It generates a component based on your team's conventions and best practices, including TypeScript types, Jest tests, Storybook stories, and Sass styles. Still used by many teams within Apple.",
         ],
     },
@@ -72,22 +72,10 @@ const experience = [
             },
         ],
         list: [
-            'Worked with a team of 13 developers to build 3D software for the U.S. Air Force',
-            'Created a tools for generating C# source code files to implement bit-level MIL-STD 6016 Tactical Data Link (TDL) protocols',
+            'Worked with a team of 13 developers to build 3D software for the U.S. Air Force.',
+            'Created a tools for generating C# source code files to implement bit-level MIL-STD 6016 Tactical Data Link (TDL) protocols.',
         ],
     },
-    // {
-    //     company: 'Robi',
-    //     site: 'https://github.com/robijs',
-    //     roles: [
-    //         {
-    //             title: 'Software Engineer',
-    //             start: 'Apr 2022',
-    //             end: 'May 2022',
-    //         },
-    //     ],
-    //     list: ['Ported Robi to React with support for PnPjs', 'Open sourced Robi'],
-    // },
     {
         company: 'sabbatical',
         roles: [
@@ -112,22 +100,10 @@ const experience = [
             /*html*/ `
                 Developed client-side web apps SharePoint 2016 with 
                 <a href="https://github.com/robijs/" target="_blank">Robi</a>, 
-                a client-side SharePoint app framework
+                a client-side SharePoint app framework.
             `,
         ],
     },
-    // {
-    //     company: 'Robi',
-    //     site: 'https://github.com/robijs',
-    //     roles: [
-    //         {
-    //             title: 'Software Engineer',
-    //             start: 'Oct 2017',
-    //             end: 'Mar 2018',
-    //         },
-    //     ],
-    //     list: ['Created Robi, a client-side SharePoint app framework', 'Freelance work'],
-    // },
     {
         company: 'sabbatical',
         roles: [
@@ -148,9 +124,9 @@ const experience = [
             },
         ],
         list: [
-            'Built client-side apps for SharePoint 2013 and 16 with jQuery, DataTables, and Chart.js',
-            'Upgraded the SharePoint 2013 environment to SharePoint 2016',
-            'Migrated the Lotus Notes environment to SharePoint 2016F',
+            'Built client-side apps for SharePoint 2013 and 16 with jQuery, DataTables, and Chart.js.',
+            'Upgraded the SharePoint 2013 environment to SharePoint 2016.',
+            'Migrated the Lotus Notes environment to SharePoint 2016.',
         ],
     },
     {
@@ -164,9 +140,8 @@ const experience = [
             },
         ],
         list: [
-            'Led a team of five developers at the Executive Office of the President responsible for modernizing their SharePoint user experience and helping the National Archives and Records Administration build tools to export records prior to the 2017 administration transition.',
-            'Built modern client-side apps for SharePoint 2013 with jQuery, DataTables, and Chart.js',
-            'Developed a custom SharePoint app framework',
+            'Led a team of five developers at the Executive Office of the President where we modernized their SharePoint user experience and worked with the National Archives and Records Administration (NARA) to build tools that export records before the next administration took office.',
+            'Built modern client-side apps for SharePoint 2013 with jQuery, DataTables, and Chart.js.',
         ],
     },
 ];
