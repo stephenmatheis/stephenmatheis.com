fda12a7c205f5926f8bc853b5c5320fb05f94d20
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-07-21 17:32:15 -0400

    chore: remove page

--- app/grid/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './page.module.scss';
-
-export default function ResumePage() {
-    return (
-        <div className={styles.page}>
-            <div className={styles.grid}></div>
-        </div>
-    );
-}
