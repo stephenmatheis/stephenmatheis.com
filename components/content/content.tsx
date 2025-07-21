0c8260884e55f4fe82eb42d40198bc86eac78292
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-07-21 18:50:29 -0400

    fix: spacing

--- components/content/content.tsx
+++ components/content/content.tsx
@@ -4,10 +4,21 @@ import { Contact } from '@/components/contact';
 import { Skills } from '@/components/skills';
 import { Projects } from '@/components/projects';
 import styles from './content.module.scss';
+import Link from 'next/link';
 
 export function Content() {
     return (
         <main className={styles.content}>
+            {/* Breadcrumbs */}
+            <div className={styles.breadcrumbs}>
+                {['app', 'page', 'resume'].map((crumb, i, arr) => (
+                    <span key={i}>
+                        <Link href="/">{crumb}</Link>
+                        {i < arr.length - 1 && ' > '}
+                    </span>
+                ))}
+            </div>
+
             {/* Lines */}
             <div className={styles.lines}>
                 {Array.from({ length: 58 }, (_, i) => (
@@ -15,6 +26,19 @@ export function Content() {
                         {i + 1}
                     </div>
                 ))}
+            </div>
+
+            {/* Status Bar */}
+            <div className={styles.statusbar}>
+                <div className={styles.buttons}>
+                    <button>Print</button>
+                    <button>System</button>
+                    <button>Light</button>
+                    <button>Dark</button>
+                </div>
+                <div className={styles.status}>
+                    <button>Status #1</button>
+                </div>
             </div>
 
             <div className={styles.left}>
