94e122840288c60e70ff2bf1d7c303f54b888cbe
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-07-21 17:25:28 -0400

    feat: remove footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import { Page } from '@/components/page';
 import { Content } from '@/components/content';
-import { Footer } from '@/components/footer';
 
 export default function ResumePage() {
     return (
         <Page>
             <Content />
-            {/* <Footer /> */}
         </Page>
     );
 }
