620d892ccaa6da2b4aaa3b8fb0c3afc6687790e8
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-07-24 16:46:20 -0400

    feat: barcode

--- components/two-zero-three-five/two-zero-three-five.tsx
+++ components/two-zero-three-five/two-zero-three-five.tsx
@@ -5,30 +5,95 @@ type TwoZeroThreeFiveProps = {};
 export function TwoZeroThreeFive({}: TwoZeroThreeFiveProps) {
     return (
         <div className={styles['two-zero-three-five']}>
-            {/* 2 */}
-            <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M44 44H0V16H32V12H0V0H44V28H12V32H44V44Z" fill="currentColor" />
-            </svg>
+            <div className={styles.barcode}>
+                {/* Start (*) */}
+                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M40 0H44V44H40V0Z" fill="#D9D9D9" />
+                    <path d="M0 0H4V44H0V0Z" fill="#D9D9D9" />
+                    <path d="M29 0H37V44H29V0Z" fill="#D9D9D9" />
+                    <path d="M18 0H26V44H18V0Z" fill="#D9D9D9" />
+                    <path d="M11 0H15V44H11V0Z" fill="#D9D9D9" />
+                </svg>
 
-            {/* 0 */}
-            <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M44 44H0V0H44V44ZM12 12V32H32V12H12Z"
-                    fill="currentColor"
-                />
-            </svg>
+                {/* 2 */}
+                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4 44H0V0H4V44Z" fill="#D9D9D9" />
+                    <path d="M15 44H7V0H15V44Z" fill="#D9D9D9" />
+                    <path d="M26 44H22V0H26V44Z" fill="#D9D9D9" />
+                    <path d="M33 44H29V0H33V44Z" fill="#D9D9D9" />
+                    <path d="M44 44H36V0H44V44Z" fill="#D9D9D9" />
+                </svg>
 
-            {/* 3 */}
-            <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M44 44H0V32H32V28H12V16H32V12H0V0H44V44Z" fill="currentColor" />
-            </svg>
+                {/* 0 */}
+                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 0H4V44H0V0Z" fill="#D9D9D9" />
+                    <path d="M40 0H44V44H40V0Z" fill="#D9D9D9" />
+                    <path d="M29 0H37V44H29V0Z" fill="#D9D9D9" />
+                    <path d="M18 0H26V44H18V0Z" fill="#D9D9D9" />
+                    <path d="M7 0H11V44H7V0Z" fill="#D9D9D9" />
+                </svg>
 
-            {/* 5 */}
-            <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M44 44H0V32H32V28H0V0H44V12H12V16H44V44Z" fill="currentColor" />
-            </svg>
+                {/* 3 */}
+                <svg width="44" height="45" viewBox="0 0 44 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M40 0.736328H44V44.7363H40V0.736328Z" fill="#D9D9D9" />
+                    <path d="M11 0.736328H19V44.7363H11V0.736328Z" fill="#D9D9D9" />
+                    <path d="M0 0.736328H8V44.7363H0V0.736328Z" fill="#D9D9D9" />
+                    <path d="M33 0.736328H37V44.7363H33V0.736328Z" fill="#D9D9D9" />
+                    <path d="M26 0.736328H30V44.7363H26V0.736328Z" fill="#D9D9D9" />
+                </svg>
+
+                {/* 5 */}
+                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M40 0H44V44H40V0Z" fill="#D9D9D9" />
+                    <path d="M0 0H8V44H0V0Z" fill="#D9D9D9" />
+                    <path d="M33 0H37V44H33V0Z" fill="#D9D9D9" />
+                    <path d="M11 0H15V44H11V0Z" fill="#D9D9D9" />
+                    <path d="M22 0H30V44H22V0Z" fill="#D9D9D9" />
+                </svg>
+
+                {/* End (*) */}
+                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M40 0H44V44H40V0Z" fill="#D9D9D9" />
+                    <path d="M0 0H4V44H0V0Z" fill="#D9D9D9" />
+                    <path d="M29 0H37V44H29V0Z" fill="#D9D9D9" />
+                    <path d="M18 0H26V44H18V0Z" fill="#D9D9D9" />
+                    <path d="M11 0H15V44H11V0Z" fill="#D9D9D9" />
+                </svg>
+            </div>
+            <div className={styles.digits}>
+                {/* Space */}
+                {/* <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg"></svg> */}
+                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="0.5" y="0.5" width="43" height="43" stroke="currentColor" />
+                </svg>
+
+                {/* 2 */}
+                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M44 44H0V16H32V12H0V0H44V28H12V32H44V44Z" fill="currentColor" />
+                </svg>
+                {/* 0 */}
+                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M44 44H0V0H44V44ZM12 12V32H32V12H12Z"
+                        fill="currentColor"
+                    />
+                </svg>
+                {/* 3 */}
+                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M44 44H0V32H32V28H12V16H32V12H0V0H44V44Z" fill="currentColor" />
+                </svg>
+                {/* 5 */}
+                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M44 44H0V32H32V28H0V0H44V12H12V16H44V44Z" fill="currentColor" />
+                </svg>
+                {/* Space */}
+                {/* <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg"></svg> */}
+                <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="0.5" y="0.5" width="43" height="43" stroke="currentColor" />
+                </svg>
+            </div>
         </div>
     );
 }
