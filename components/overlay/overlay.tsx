c751513f8513bb1ac052ec7dc3e63c661e256a5f
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-07-28 09:32:34 -0400

    feat: navigation

--- components/overlay/overlay.tsx
+++ components/overlay/overlay.tsx
@@ -9,7 +9,7 @@ export function Overlay() {
     const [viewport, setViewport] = useState({ width: 0, height: 0 });
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
     const { overlays, setOverlays } = useOverlay();
-    const isAllOn = useMemo(() => Object.values(overlays).every((value) => value), [overlays]);
+    const isAllOn = useMemo(() => Object.values(overlays).every(({ isOn }) => isOn), [overlays]);
 
     useEffect(() => {
         function updateMousePosition(event: PointerEvent) {
@@ -119,12 +119,41 @@ export function Overlay() {
                     ].map(({ key, name, value, alt }) => (
                         <button
                             key={key}
-                            className={`${styles.item}${overlays[key as keyof Overlay] ? ` ${styles.on}` : ''}`}
+                            className={`${styles.item}${
+                                overlays[key as keyof Overlay].isHovered || overlays[key as keyof Overlay].isOn
+                                    ? ` ${styles.on}`
+                                    : ''
+                            }`}
                             onClick={() => {
                                 setOverlays((prev) => ({
                                     ...prev,
-                                    [key]: !prev[key as keyof Overlay],
+                                    [key]: {
+                                        ...prev[key as keyof Overlay],
+                                        isOn: !prev[key as keyof Overlay].isOn,
+                                    },
                                 }));
+                            }}
+                            onMouseOver={() => {
+                                setOverlays((prev) => {
+                                    return {
+                                        ...prev,
+                                        [key]: {
+                                            ...prev[key as keyof Overlay],
+                                            isHovered: true,
+                                        },
+                                    };
+                                });
+                            }}
+                            onMouseLeave={() => {
+                                setOverlays((prev) => {
+                                    return {
+                                        ...prev,
+                                        [key]: {
+                                            ...prev[key as keyof Overlay],
+                                            isHovered: false,
+                                        },
+                                    };
+                                });
                             }}
                         >
                             <span className={styles.name}>{name}</span>
@@ -150,11 +179,38 @@ export function Overlay() {
                     ].map(({ key, name }) => (
                         <button
                             key={key}
-                            className={`${styles.item}${overlays[key as keyof Overlay] ? ` ${styles.on}` : ''}`}
+                            className={`${styles.item}${
+                                overlays[key as keyof Overlay].isHovered || overlays[key as keyof Overlay].isOn
+                                    ? ` ${styles.on}`
+                                    : ''
+                            }`}
                             onClick={() => {
                                 setOverlays((prev) => ({
                                     ...prev,
-                                    [key]: !prev[key as keyof Overlay],
+                                    [key]: {
+                                        ...prev[key as keyof Overlay],
+                                        isOn: !prev[key as keyof Overlay].isOn,
+                                    },
+                                }));
+                            }}
+                            onMouseOver={() => {
+                                setOverlays((prev) => {
+                                    return {
+                                        ...prev,
+                                        [key]: {
+                                            ...prev[key as keyof Overlay],
+                                            isHovered: true,
+                                        },
+                                    };
+                                });
+                            }}
+                            onMouseLeave={() => {
+                                setOverlays((prev) => ({
+                                    ...prev,
+                                    [key]: {
+                                        ...prev[key as keyof Overlay],
+                                        isHovered: false,
+                                    },
                                 }));
                             }}
                         >
@@ -179,11 +235,38 @@ export function Overlay() {
                     ].map(({ key, name }) => (
                         <button
                             key={key}
-                            className={`${styles.item}${overlays[key as keyof Overlay] ? ` ${styles.on}` : ''}`}
+                            className={`${styles.item}${
+                                overlays[key as keyof Overlay].isHovered || overlays[key as keyof Overlay].isOn
+                                    ? ` ${styles.on}`
+                                    : ''
+                            }`}
                             onClick={() => {
                                 setOverlays((prev) => ({
                                     ...prev,
-                                    [key]: !prev[key as keyof Overlay],
+                                    [key]: {
+                                        ...prev[key as keyof Overlay],
+                                        isOn: !prev[key as keyof Overlay].isOn,
+                                    },
+                                }));
+                            }}
+                            onMouseOver={() => {
+                                setOverlays((prev) => {
+                                    return {
+                                        ...prev,
+                                        [key]: {
+                                            ...prev[key as keyof Overlay],
+                                            isHovered: true,
+                                        },
+                                    };
+                                });
+                            }}
+                            onMouseLeave={() => {
+                                setOverlays((prev) => ({
+                                    ...prev,
+                                    [key]: {
+                                        ...prev[key as keyof Overlay],
+                                        isHovered: false,
+                                    },
                                 }));
                             }}
                         >
@@ -197,11 +280,29 @@ export function Overlay() {
                             setOverlays((prev) => {
                                 return Object.fromEntries(
                                     Object.entries(prev).map(([key, _]) => {
-                                        return [key, isAllOn ? false : true];
+                                        return [key, isAllOn ? { ..._, isOn: false } : { ..._, isOn: true }];
                                     })
                                 ) as Overlay;
                             })
                         }
+                        onMouseOver={() => {
+                            setOverlays((prev) => {
+                                return Object.fromEntries(
+                                    Object.entries(prev).map(([key, _]) => {
+                                        return [key, { ..._, isHovered: true }];
+                                    })
+                                ) as Overlay;
+                            });
+                        }}
+                        onMouseLeave={() => {
+                            setOverlays((prev) => {
+                                return Object.fromEntries(
+                                    Object.entries(prev).map(([key, _]) => {
+                                        return [key, { ..._, isHovered: false }];
+                                    })
+                                ) as Overlay;
+                            });
+                        }}
                     >
                         <span className={styles.name}>All</span>
                     </button>
@@ -241,10 +342,14 @@ export function Overlay() {
                 {/* Bottom Right */}
                 <div className={styles.bottomright}></div>
             </div>
-            <div className={`${styles.page}${overlays.page ? ` ${styles.on}` : ''}`}>
+            <div className={`${styles.page}${overlays.page.isOn || overlays.page.isHovered ? ` ${styles.on}` : ''}`}>
                 <main className={styles.content}>
                     {/* Character Width */}
-                    <div className={`${styles.charWidth}${overlays.charWidth ? ` ${styles.on}` : ''}`}>
+                    <div
+                        className={`${styles.charWidth}${
+                            overlays.charWidth.isOn || overlays.charWidth.isHovered ? ` ${styles.on}` : ''
+                        }`}
+                    >
                         <svg width="7" height="2" viewBox="0 0 7 2" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect y="1" width="7" height="1" fill="currentColor" />
                             <rect width="1" height="1" fill="currentColor" />
@@ -254,7 +359,11 @@ export function Overlay() {
                     </div>
 
                     {/* Font Size */}
-                    <div className={`${styles.fontsize}${overlays.fontSize ? ` ${styles.on}` : ''}`}>
+                    <div
+                        className={`${styles.fontsize}${
+                            overlays.fontSize.isOn || overlays.fontSize.isHovered ? ` ${styles.on}` : ''
+                        }`}
+                    >
                         <svg width="2" height="11" viewBox="0 0 2 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect x="1" width="1" height="11" fill="currentColor" />
                             <rect width="1" height="1" fill="currentColor" />
@@ -264,7 +373,11 @@ export function Overlay() {
                     </div>
 
                     {/* Line Height */}
-                    <div className={`${styles.lineheight}${overlays.lineHeight ? ` ${styles.on}` : ''}`}>
+                    <div
+                        className={`${styles.lineheight}${
+                            overlays.lineHeight.isOn || overlays.lineHeight.isHovered ? ` ${styles.on}` : ''
+                        }`}
+                    >
                         <svg width="3" height="16.5" viewBox="0 0 3 16.5" xmlns="http://www.w3.org/2000/svg">
                             <rect x="0" y="0" width="1" height="1" fill="currentColor" />
                             <rect x="2" y="0" width="1" height="1" fill="currentColor" />
@@ -276,7 +389,11 @@ export function Overlay() {
                     </div>
 
                     {/* Left Column Width */}
-                    <div className={`${styles.leftwidth}${overlays.leftWidth ? ` ${styles.on}` : ''}`}>
+                    <div
+                        className={`${styles.leftwidth}${
+                            overlays.leftWidth.isOn || overlays.leftWidth.isHovered ? ` ${styles.on}` : ''
+                        }`}
+                    >
                         <svg width="392" height="7" viewBox="0 0 392 7" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M4 7H3V6H4V7ZM389 7H388V6H389V7ZM3 6H2V5H3V6ZM390 6H389V5H390V6ZM2 3H390V2H391V3H392V4H391V5H390V4H2V5H1V4H0V3H1V2H2V3ZM3 2H2V1H3V2ZM390 2H389V1H390V2ZM4 1H3V0H4V1ZM389 1H388V0H389V1Z"
@@ -287,7 +404,9 @@ export function Overlay() {
                     </div>
 
                     {/* Gap */}
-                    <div className={`${styles.gap}${overlays.gap ? ` ${styles.on}` : ''}`}>
+                    <div
+                        className={`${styles.gap}${overlays.gap.isOn || overlays.gap.isHovered ? ` ${styles.on}` : ''}`}
+                    >
                         <svg width="28" height="3" viewBox="0 0 28 3" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1 1H27V0H28V3H27V2H1V3H0V0H1V1Z" fill="currentColor" />
                         </svg>
@@ -295,7 +414,11 @@ export function Overlay() {
                     </div>
 
                     {/* Right Column Width */}
-                    <div className={`${styles.rightwidth}${overlays.rightWidth ? ` ${styles.on}` : ''}`}>
+                    <div
+                        className={`${styles.rightwidth}${
+                            overlays.rightWidth.isOn || overlays.rightWidth.isHovered ? ` ${styles.on}` : ''
+                        }`}
+                    >
                         <svg width="245" height="7" viewBox="0 0 245 7" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path
                                 d="M4 7H3V6H4V7ZM242 7H241V6H242V7ZM3 6H2V5H3V6ZM243 6H242V5H243V6ZM2 3H243V2H244V3H245V4H244V5H243V4H2V5H1V4H0V3H1V2H2V3ZM3 2H2V1H3V2ZM243 2H242V1H243V2ZM4 1H3V0H4V1ZM242 1H241V0H242V1Z"
@@ -305,12 +428,24 @@ export function Overlay() {
                         <div className={styles.label}>35ch</div>
                     </div>
 
-                    <div className={`${styles.left}${overlays.left ? ` ${styles.on}` : ''}`} />
-                    <div className={`${styles.right}${overlays.right ? ` ${styles.on}` : ''}`} />
+                    <div
+                        className={`${styles.left}${
+                            overlays.left.isOn || overlays.left.isHovered ? ` ${styles.on}` : ''
+                        }`}
+                    />
+                    <div
+                        className={`${styles.right}${
+                            overlays.right.isOn || overlays.right.isHovered ? ` ${styles.on}` : ''
+                        }`}
+                    />
                 </main>
 
                 {/* Page Padding */}
-                <div className={`${styles.paddingbottom}${overlays.paddingBottom ? ` ${styles.on}` : ''}`}>
+                <div
+                    className={`${styles.paddingbottom}${
+                        overlays.paddingBottom.isOn || overlays.paddingBottom.isHovered ? ` ${styles.on}` : ''
+                    }`}
+                >
                     <svg width="3" height="50" viewBox="0 0 3 50" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M3 0V1H2V48.5H3V49.5H0V48.5H1V1H0V0H3Z" fill="currentColor" />
                     </svg>
@@ -318,7 +453,11 @@ export function Overlay() {
                 </div>
 
                 {/* Page Padding */}
-                <div className={`${styles.paddingtop}${overlays.paddingTop ? ` ${styles.on}` : ''}`}>
+                <div
+                    className={`${styles.paddingtop}${
+                        overlays.paddingTop.isOn || overlays.paddingTop.isHovered ? ` ${styles.on}` : ''
+                    }`}
+                >
                     <svg width="3" height="50" viewBox="0 0 3 50" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M3 0V1H2V48.5H3V49.5H0V48.5H1V1H0V0H3Z" fill="currentColor" />
                     </svg>
@@ -326,7 +465,11 @@ export function Overlay() {
                 </div>
 
                 {/* Content Height */}
-                <div className={`${styles.contentheight}${overlays.contentHeight ? ` ${styles.on}` : ''}`}>
+                <div
+                    className={`${styles.contentheight}${
+                        overlays.contentHeight.isOn || overlays.contentHeight.isHovered ? ` ${styles.on}` : ''
+                    }`}
+                >
                     <svg width="7" height="957" viewBox="0 0 7 957" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M4 1H5V2H4V955H5V956H4V957H3V956H2V955H3V2H2V1H3V0H4V1Z" fill="currentColor" />
                         <path d="M2 955H1V954H2V955Z" fill="currentColor" />
@@ -342,7 +485,11 @@ export function Overlay() {
                 </div>
 
                 {/* Content Width */}
-                <div className={`${styles.contentwidth}${overlays.contentWidth ? ` ${styles.on}` : ''}`}>
+                <div
+                    className={`${styles.contentwidth}${
+                        overlays.contentWidth.isOn || overlays.contentWidth.isHovered ? ` ${styles.on}` : ''
+                    }`}
+                >
                     <svg width="665" height="7" viewBox="0 0 665 7" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M4 7H3V6H4V7ZM662 7H661V6H662V7ZM3 6H2V5H3V6ZM663 6H662V5H663V6ZM2 3H663V2H664V3H665V4H664V5H663V4H2V5H1V4H0V3H1V2H2V3ZM3 2H2V1H3V2ZM663 2H662V1H663V2ZM4 1H3V0H4V1ZM662 1H661V0H662V1Z"
@@ -353,7 +500,11 @@ export function Overlay() {
                 </div>
 
                 {/* Page Height */}
-                <div className={`${styles.pageheight}${overlays.pageHeight ? ` ${styles.on}` : ''}`}>
+                <div
+                    className={`${styles.pageheight}${
+                        overlays.pageHeight.isOn || overlays.pageHeight.isHovered ? ` ${styles.on}` : ''
+                    }`}
+                >
                     <svg width="9" height="1056" viewBox="0 0 9 1056" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M3 1056H0V1055H3V1056Z" fill="currentColor" />
                         <path d="M5 1055H3V1054H5V1055Z" fill="currentColor" />
@@ -371,7 +522,11 @@ export function Overlay() {
                 </div>
 
                 {/* Page Width */}
-                <div className={`${styles.pagewidth}${overlays.pageWidth ? ` ${styles.on}` : ''}`}>
+                <div
+                    className={`${styles.pagewidth}${
+                        overlays.pageWidth.isOn || overlays.pageWidth.isHovered ? ` ${styles.on}` : ''
+                    }`}
+                >
                     <svg width="816" height="9" viewBox="0 0 816 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M808 9H8V8H808V9Z" fill="currentColor" />
                         <path d="M8 8H5V7H8V8Z" fill="currentColor" />
