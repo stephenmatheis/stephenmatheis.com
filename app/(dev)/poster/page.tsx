3369278dad85719827cb26c142c6c8613ed23496
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-07-29 11:05:47 -0400

    feat: poster page (WIP)

---
+++ app/(dev)/poster/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss';
+
+export default function PosterPage() {
+    return (
+        <div className={styles.poster}>
+            <h1 className={styles.h1}>Poster</h1>
+        </div>
+    );
+}
