98061cad775096897fc4df8a218fb8e86af8e0c1
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-08-01 10:19:56 -0400

    feat: cursor

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import Link from 'next/link';
+import * as motion from 'motion/react-client';
+import { useCursor } from '@/providers/cursor';
 import { Section } from '@/components/section';
 import contact from '@/data/contact';
 import styles from './contact.module.scss';
 
 export function Contact() {
+    const { setLeft, setWidth, setGrow } = useCursor();
+
     return (
         <Section className={styles.contact} heading="Contact">
             <div className={styles.list}>
@@ -11,9 +17,29 @@ export function Contact() {
                     return (
                         <div key={index} className={styles.item}>
                             <div className={styles.name}>{item.name}</div>
-                            <div className={styles.text}>
+                            <motion.div
+                                className={styles.text}
+                                onHoverStart={(event) => {
+                                    const rect = (event.target as HTMLElement)
+                                        .querySelector('a')
+                                        ?.getBoundingClientRect();
+
+                                    if (!rect) return;
+
+                                    const { width, left } = rect;
+
+                                    setLeft(left);
+                                    setWidth(width);
+                                    setGrow('link');
+                                }}
+                                onHoverEnd={() => {
+                                    setLeft(0);
+                                    setWidth(0);
+                                    setGrow('normal');
+                                }}
+                            >
                                 <Link href={item.href}>{item.user || item.text}</Link>
-                            </div>
+                            </motion.div>
                         </div>
                     );
                 })}
