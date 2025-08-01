8e52ca9cbd39669f8a7fbadcb8d495148d07b9bf
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-08-01 12:38:37 -0400

    feat: cursor (statusbar links)

--- components/viewport/viewport.tsx
+++ components/viewport/viewport.tsx
@@ -117,43 +117,97 @@ export function Viewport({ children }: { children: React.ReactNode }) {
                         }`}
                     >
                         <div className={styles.block}>
-                            <Link href="https://github.com/stephenmatheis/stephenmatheis.com/tree/main" target="_blank">
-                                <svg
-                                    width="11"
-                                    height="17"
-                                    viewBox="0 0 11 17"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
+                            <motion.span
+                                onHoverStart={(event) => {
+                                    const rect = (event.target as HTMLElement)
+                                        .querySelector('a')
+                                        ?.getBoundingClientRect();
+
+                                    if (!rect) return;
+
+                                    const { width, left } = rect;
+
+                                    setLeft(left);
+                                    setWidth(width);
+                                    setGrow('link');
+                                }}
+                                onHoverEnd={() => {
+                                    setLeft(0);
+                                    setWidth(0);
+                                    setGrow('normal');
+                                }}
+                            >
+                                <Link
+                                    href="https://github.com/stephenmatheis/stephenmatheis.com/tree/main"
+                                    target="_blank"
                                 >
-                                    <path d="M4 17H1V16H4V17Z" fill="currentColor" />
-                                    <path d="M1 16H0V13H1V16Z" fill="currentColor" />
-                                    <path d="M5 16H4V13H5V16Z" fill="currentColor" />
-                                    <path d="M4 5H3V10H4V11H3V12H4V13H1V12H2V5H1V4H4V5Z" fill="currentColor" />
-                                    <path d="M8 10H4V9H8V10Z" fill="currentColor" />
-                                    <path d="M10 8H9V9H8V8H7V7H10V8Z" fill="currentColor" />
-                                    <path d="M7 7H6V4H7V7Z" fill="currentColor" />
-                                    <path d="M11 7H10V4H11V7Z" fill="currentColor" />
-                                    <path d="M1 4H0V1H1V4Z" fill="currentColor" />
-                                    <path d="M5 4H4V1H5V4Z" fill="currentColor" />
-                                    <path d="M10 4H7V3H10V4Z" fill="currentColor" />
-                                    <path d="M4 1H1V0H4V1Z" fill="currentColor" />
-                                </svg>
-                                main*
-                            </Link>
+                                    <svg
+                                        width="11"
+                                        height="17"
+                                        viewBox="0 0 11 17"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d="M4 17H1V16H4V17Z" fill="currentColor" />
+                                        <path d="M1 16H0V13H1V16Z" fill="currentColor" />
+                                        <path d="M5 16H4V13H5V16Z" fill="currentColor" />
+                                        <path d="M4 5H3V10H4V11H3V12H4V13H1V12H2V5H1V4H4V5Z" fill="currentColor" />
+                                        <path d="M8 10H4V9H8V10Z" fill="currentColor" />
+                                        <path d="M10 8H9V9H8V8H7V7H10V8Z" fill="currentColor" />
+                                        <path d="M7 7H6V4H7V7Z" fill="currentColor" />
+                                        <path d="M11 7H10V4H11V7Z" fill="currentColor" />
+                                        <path d="M1 4H0V1H1V4Z" fill="currentColor" />
+                                        <path d="M5 4H4V1H5V4Z" fill="currentColor" />
+                                        <path d="M10 4H7V3H10V4Z" fill="currentColor" />
+                                        <path d="M4 1H1V0H4V1Z" fill="currentColor" />
+                                    </svg>
+                                    main*
+                                </Link>
+                            </motion.span>
                         </div>
                         <div className={styles.block}>
-                            <Link href="https://www.unicode.org/versions/Unicode8.0.0/" target="_blank">
-                                UTF-8
-                            </Link>
-                            <Link
-                                href="https://docs.github.com/en/get-started/git-basics/configuring-git-to-handle-line-endings"
-                                target="_blank"
-                            >
-                                LF
-                            </Link>
-                            <Link href="https://en.wikipedia.org/wiki/Plain_text" target="_blank">
-                                {'¶'} Plain Text
-                            </Link>
+                            {[
+                                {
+                                    label: 'UTF-8',
+                                    href: 'https://www.unicode.org/versions/Unicode8.0.0/',
+                                },
+                                {
+                                    label: 'LF',
+                                    href: 'https://docs.github.com/en/get-started/git-basics/configuring-git-to-handle-line-endings',
+                                },
+                                {
+                                    label: '¶ Plain Text',
+                                    href: 'https://en.wikipedia.org/wiki/Plain_text',
+                                },
+                            ].map(({ label, href }) => {
+                                return (
+                                    <motion.span
+                                        key={label}
+                                        onHoverStart={(event) => {
+                                            const rect = (event.target as HTMLElement)
+                                                .querySelector('a')
+                                                ?.getBoundingClientRect();
+
+                                            if (!rect) return;
+
+                                            const { width, left } = rect;
+
+                                            setLeft(left);
+                                            setWidth(width);
+                                            setGrow('link');
+                                        }}
+                                        onHoverEnd={() => {
+                                            setLeft(0);
+                                            setWidth(0);
+                                            setGrow('normal');
+                                        }}
+                                    >
+                                        <Link href={href} target="_blank">
+                                            {label}
+                                        </Link>
+                                    </motion.span>
+                                );
+                            })}
                         </div>
                     </div>
                 </Page>
