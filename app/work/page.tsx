082e891cb4b6d8a22aa4177f34636d8db27e57d9
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-08-07 17:12:05 -0400

    feat: glass effect w/ noisy background (WIP)

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,7 @@
 export default function WorkPage() {
     return (
         <>
-            <h2>🚧 Under development</h2>
+            <h2>{'</>'} Under development</h2>
         </>
     );
 }
--- components/viewport/viewport.tsx
+++ components/viewport/viewport.tsx
@@ -4,6 +4,8 @@ import styles from './viewport.module.scss';
 export function Viewport({ children }: { children: React.ReactNode }) {
     return (
         <div className={styles.viewport}>
+            {/* <div className={styles.background} />
+            <div className={styles.foreground} /> */}
             <Sheet>{children}</Sheet>
         </div>
     );
