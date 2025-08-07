ae5ba9e2dad4803089690285b6380dcf92dad387
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-08-06 22:38:37 -0400

    fix: routes (WIP)

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-import { Viewport } from '@/components/viewport';
-import { Content } from '@/components/content';
-import styles from './page.module.scss';
-
 export default function RootPage() {
     return (
-        <Viewport>
-            <Content>
-                <div className={styles.root}>
-                    <p>hi</p>
-                    <p>line 2</p>
-                </div>
-            </Content>
-        </Viewport>
+        <>
+            <h2>Home</h2>
+        </>
     );
 }
