c2a84ca2fd84279e76a86e62fc32c237900b3cd6
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-08-20 11:39:25 -0400

    fix: build error

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,9 @@
-import { PageControls } from '@/components/page-controls';
 import { TableOfContents } from '@/components/table-of-contents';
 
 export default function RootPage() {
     return (
         <>
             <TableOfContents />
-            <PageControls />
         </>
     );
 }
