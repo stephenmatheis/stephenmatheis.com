de0d0cca17e79e4eb91093abbfca87dc7d257713
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-08-27 15:59:20 -0400

    chore: delete print view

--- app/(print)/components/comment/comment.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './comment.module.scss';
-
-export function Comment({ text, ...props }) {
-    return (
-        <h2 className={styles.comment} {...props}>
-            {text}
-        </h2>
-    );
-}
