e94366ec730bc5c23f8099c0c9b22cf2d82e76ff
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-09-06 14:54:12 -0400

    feat: contact

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -7,15 +7,12 @@ export function Contact() {
             <a href="mailto:stephen@matheis.email" target="_blank" title="My email address">
                 stephen@matheis.email
             </a>
-            {' | '}
             <a href="tel:9124922522" target="_blank" title="My phone number">
                 (912) 492-2522
             </a>
-            {' | '}
             <a href="https://github.com/stephenmatheis" target="_blank" title="My GitHub profile">
                 github.com/stephenmatheis
             </a>
-            {' | '}
             <a href="https://stephenmatheis.com" target="_blank" title="My website">
                 stephenmatheis.com
             </a>
