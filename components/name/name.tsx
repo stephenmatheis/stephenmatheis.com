cf6d68eaa24ca06b15772ea0e93992aa5a21f73b
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-09-09 14:55:58 -0400

    feat: update resume copy

--- components/name/name.tsx
+++ components/name/name.tsx
@@ -2,10 +2,9 @@ import styles from './name.module.scss';
 
 export function Name() {
     return (
-        <h1 className={styles.name}>
-            Stephen Matheis
-            <br />
-            <span>Software Engineer</span>
-        </h1>
+        <div className={styles.name}>
+            <h1>Stephen Matheis</h1>
+            <p>Software Engineer</p>
+        </div>
     );
 }
