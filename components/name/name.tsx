a0a38372dfd57be0116698362d6e6ccd8b63ac02
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-10-03 17:37:51 -0400

    feat: remove experience details

--- components/name/name.tsx
+++ components/name/name.tsx
@@ -1,10 +1,5 @@
 import styles from './name.module.scss';
 
 export function Name() {
-    return (
-        <div className={styles.name}>
-            <h1>Stephen Matheis</h1>
-            <p>Software Engineer</p>
-        </div>
-    );
+    return <h1 className={styles.name}>Stephen Matheis</h1>;
 }
