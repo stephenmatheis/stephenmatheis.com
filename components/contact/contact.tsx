026d3675afd9022d2c00b67d0282a79a17b36225
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-10-03 17:47:18 -0400

    Update contact.tsx

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,24 +1,24 @@
-import styles from './contact.module.scss';
-
 export function Contact() {
     return (
-        <div className={styles.contact}>
+        <>
             <h2>Contact</h2>
-            <p>
-                <a href="mailto:stephen@matheis.email" target="_blank" title="My email address">
-                    stephen@matheis.email
-                </a>
-            </p>
-            <p>
-                <a href="https://github.com/stephenmatheis" target="_blank" title="My GitHub profile">
-                    github.com/stephenmatheis
-                </a>
-            </p>
-            <p>
-                <a href="https://stephenmatheis.com" target="_blank" title="My website">
-                    stephenmatheis.com
-                </a>
-            </p>
-        </div>
+            <ul>
+                <li>
+                    <a href="mailto:stephen@matheis.email" target="_blank" title="My email address">
+                        stephen@matheis.email
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/stephenmatheis" target="_blank" title="My GitHub profile">
+                        github.com/stephenmatheis
+                    </a>
+                </li>
+                <li>
+                    <a href="https://stephenmatheis.com" target="_blank" title="My website">
+                        stephenmatheis.com
+                    </a>
+                </li>
+            </ul>
+        </>
     );
 }
