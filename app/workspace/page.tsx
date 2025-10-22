be4449dab50ea2defcbbe95e6079c061389c8c68
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-10-22 08:20:03 -0400

    feat: simple

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -1,16 +1,62 @@
-import { Name } from '@/components/name';
-import { Experience } from '@/components/experience';
-import { Contact } from '@/components/contact';
+import { Fragment } from 'react';
+import experience from '@/data/experience';
 import styles from './page.module.scss';
+import Link from 'next/link';
 
 export default function AboutPage() {
     return (
         <div className={styles.page}>
-            <main>
-                <Name />
-                <Contact />
-                <Experience />
-            </main>
+            <div className={styles.content}>
+                <header>
+                    <h1>
+                        <Link href="/">
+                            <span>Stephen Matheis</span>
+                            <span className={styles.muted}>Software Engineer</span>
+                        </Link>
+                    </h1>
+                    <ul>
+                        <li>
+                            <a
+                                className={styles.muted}
+                                href="mailto:stephen@matheis.email"
+                                target="_blank"
+                                title="My email address"
+                            >
+                                stephen@matheis.email
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className={styles.muted}
+                                href="https://github.com/stephenmatheis"
+                                target="_blank"
+                                title="My GitHub profile"
+                            >
+                                github.com/stephenmatheis
+                            </a>
+                        </li>
+                    </ul>
+                </header>
+                <ul className={styles.roles}>
+                    {experience.map(({ company, roles }, index) => {
+                        return (
+                            <Fragment key={index}>
+                                {roles.map(({ title, start, end }, index: number) => {
+                                    return (
+                                        <li key={index}>
+                                            <span>
+                                                {start}
+                                                {end && <>-{end}</>}
+                                            </span>
+                                            <span>{title}</span>, <span className={styles.muted}>{company}</span>
+                                        </li>
+                                    );
+                                })}
+                            </Fragment>
+                        );
+                    })}
+                </ul>
+            </div>
         </div>
     );
 }
