dda7290afc8fc2bdcc91088a66911031904bff87
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-11-03 13:59:38 -0500

    feat: 3d

--- app/3d/page.tsx
+++ app/3d/page.tsx
@@ -1,23 +1,40 @@
 'use client';
 
-import { Suspense, useRef } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stage } from '@react-three/drei';
+import { Grid, OrbitControls, Stage, Environment } from '@react-three/drei';
+import { EffectComposer, Bloom, ToneMapping } from '@react-three/postprocessing';
+import { Color } from 'three';
 import Model from './Model';
 import styles from './page.module.scss';
 
 export default function Page() {
-    const ref = useRef(null);
-
     return (
         <div className={styles.page}>
-            <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
-                <Suspense fallback={null}>
-                    <Stage preset="rembrandt" intensity={1} environment="city">
-                        <Model />
-                    </Stage>
-                </Suspense>
-                <OrbitControls ref={ref} />
+            <Canvas flat shadows camera={{ position: [-15, 0, 10], fov: 25 }}>
+                <Stage
+                    intensity={0.5}
+                    shadows={{ type: 'accumulative', bias: -0.001, intensity: Math.PI }}
+                    adjustCamera={false}
+                >
+                    <Model rotation={[0, Math.PI, 0]} />
+                </Stage>
+                <Grid
+                    renderOrder={-1}
+                    position={[0, -0.9, 0]}
+                    infiniteGrid
+                    cellSize={0.6}
+                    cellThickness={0.6}
+                    sectionSize={3.3}
+                    sectionThickness={1.5}
+                    sectionColor={new Color(0.5, 0.5, 1)}
+                    fadeDistance={30}
+                />
+                <OrbitControls makeDefault minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} />
+                <EffectComposer>
+                    <Bloom luminanceThreshold={2} mipmapBlur />
+                    <ToneMapping />
+                </EffectComposer>
+                <Environment background preset="sunset" blur={0.8} />
             </Canvas>
         </div>
     );
