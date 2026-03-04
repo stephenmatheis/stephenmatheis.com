f2171e50e5850a71abccc46d45a02b0f85969845
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-03-04 09:38:19 -0500

    feat: easter eggs

--- components/Statusbar/Statusbar.tsx
+++ components/Statusbar/Statusbar.tsx
@@ -6,6 +6,43 @@ export function Statusbar() {
         <div className={styles.statusbar}>
             <div className={styles.left}>
                 <div className={`${styles.mode} ${styles.normal}`}>
+                    <span>VERSION</span>
+                </div>
+                <div className={styles.file}>
+                    <span>2.0.35</span>
+                </div>
+                {/* <div className={styles.file}>
+                    <span>resume</span>
+                </div> */}
+                <div className={styles.stats}>
+                    {/* <span className={styles.stat}>bg: #282828</span> <span className={styles.stat}>fg: #ebdbb2</span>{' '}
+                    <span className={styles.stat}>font: hack</span> <span className={styles.stat}>size: 12px</span>{' '}
+                    <span className={styles.stat}>height: 18px</span> */}
+                </div>
+            </div>
+            <div className={styles.right}>
+                {/* <div className={styles.plugins}>
+                    <svg width="16" height="16" viewBox="0 0 16 16" strokeLinejoin="round">
+                        <path
+                            d="M8 0.154663L8.34601 0.334591L14.596 3.58459L15 3.79466V4.25V11.75V12.2053L14.596 12.4154L8.34601 15.6654L8 15.8453L7.65399 15.6654L1.40399 12.4154L1 12.2053V11.75V4.25V3.79466L1.40399 3.58459L7.65399 0.334591L8 0.154663ZM2.5 11.2947V5.44058L7.25 7.81559V13.7647L2.5 11.2947ZM8.75 13.7647L13.5 11.2947V5.44056L8.75 7.81556V13.7647ZM8 1.84534L12.5766 4.22519L7.99998 6.51352L3.42335 4.2252L8 1.84534Z"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            fill="currentColor"
+                        />
+                    </svg>
+                    <span>2</span>
+                </div> */}
+                <div className={styles.outerbar}>
+                    <div className={styles.position}>hjkl</div>
+                    <div className={styles.line}>42:42</div>
+                    <div className={styles.innerbar}>
+                        <Time />
+                    </div>
+                </div>
+            </div>
+
+            {/* <div className={styles.left}>
+                <div className={`${styles.mode} ${styles.normal}`}>
                     <span>NORMAL</span>
                 </div>
                 <div className={styles.file}>
@@ -31,7 +68,7 @@ export function Statusbar() {
                         <Time />
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 }
--- components/name/name.tsx
+++ components/name/name.tsx
@@ -5,7 +5,7 @@ export function Name() {
     return (
         <Section className={styles.name} heading="">
             <h1>
-                Stephen Matheis | <span>Software Engineer</span>
+                Stephen Matheis <span>|</span> <span>Software Engineer</span>
             </h1>
             <div className={styles.muted}>Washington, D.C.</div>
             <div className={styles.muted}>TS/SCI</div>
