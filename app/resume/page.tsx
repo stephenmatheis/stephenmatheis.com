fa7672c5473f68cb1bf36eb19dfffae58346e9be
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-03-05 13:37:01 -0500

    feat: configurable statusbar

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -7,6 +7,7 @@ import { Education } from '@/components/education';
 import { About } from '@/components/About';
 import { Certifications } from '@/components/Certifications';
 import { Statusbar } from '@/components/Statusbar';
+import { Time } from '@/components/Time';
 import styles from './page.module.scss';
 
 export default function Page() {
@@ -38,7 +39,24 @@ export default function Page() {
                             <Certifications />
                         </div>
                     </div>
-                    <Statusbar />
+                    <Statusbar
+                        mode="VERSION"
+                        fileName="2.0.35"
+                        outerBar={
+                            <a className={styles.download} href="/stephen-matheis-resume.pdf" target="_blank">
+                                <svg width="12" height="12" viewBox="0 0 16 16" strokeLinejoin="round">
+                                    <path
+                                        d="M14.5 13.5V6.5V5.41421C14.5 5.149 14.3946 4.89464 14.2071 4.70711L9.79289 0.292893C9.60536 0.105357 9.351 0 9.08579 0H8H3H1.5V1.5V13.5C1.5 14.8807 2.61929 16 4 16H12C13.3807 16 14.5 14.8807 14.5 13.5ZM13 13.5V6.5H9.5H8V5V1.5H3V13.5C3 14.0523 3.44772 14.5 4 14.5H12C12.5523 14.5 13 14.0523 13 13.5ZM9.5 5V2.12132L12.3787 5H9.5ZM5.13 5.00062H4.505V6.25062H5.13H6H6.625V5.00062H6H5.13ZM4.505 8H5.13H11H11.625V9.25H11H5.13H4.505V8ZM5.13 11H4.505V12.25H5.13H11H11.625V11H11H5.13Z"
+                                        fillRule="evenodd"
+                                        clipRule="evenodd"
+                                        fill="currentColor"
+                                    />
+                                </svg>
+                                <span>Download</span>
+                            </a>
+                        }
+                        innerBar={<Time />}
+                    />
                 </div>
             </div>
         </div>
--- components/Statusbar/Statusbar.tsx
+++ components/Statusbar/Statusbar.tsx
@@ -1,34 +1,29 @@
-import { Time } from '@/components/Time';
+import { ReactNode } from 'react';
 import styles from './Statusbar.module.scss';
 
-export function Statusbar() {
+type StatusbarProps = {
+    mode: string;
+    fileName: string;
+    outerBar: ReactNode | string;
+    innerBar: ReactNode | string;
+};
+
+export function Statusbar({ mode, fileName, outerBar, innerBar }: StatusbarProps) {
     return (
         <div className={styles.statusbar}>
             <div className={styles.left}>
                 <div className={`${styles.mode} ${styles.normal}`}>
-                    <span>VERSION</span>
+                    <span>{mode}</span>
                 </div>
                 <div className={styles.file}>
-                    <span>2.0.35</span>
+                    <span>{fileName}</span>
                 </div>
             </div>
             <div className={styles.center}></div>
             <div className={styles.right}>
                 <div className={styles.outerbar}>
-                    <a className={styles.download} href="/stephen-matheis-resume.pdf" target="_blank">
-                        <svg width="12" height="12" viewBox="0 0 16 16" strokeLinejoin="round">
-                            <path
-                                d="M14.5 13.5V6.5V5.41421C14.5 5.149 14.3946 4.89464 14.2071 4.70711L9.79289 0.292893C9.60536 0.105357 9.351 0 9.08579 0H8H3H1.5V1.5V13.5C1.5 14.8807 2.61929 16 4 16H12C13.3807 16 14.5 14.8807 14.5 13.5ZM13 13.5V6.5H9.5H8V5V1.5H3V13.5C3 14.0523 3.44772 14.5 4 14.5H12C12.5523 14.5 13 14.0523 13 13.5ZM9.5 5V2.12132L12.3787 5H9.5ZM5.13 5.00062H4.505V6.25062H5.13H6H6.625V5.00062H6H5.13ZM4.505 8H5.13H11H11.625V9.25H11H5.13H4.505V8ZM5.13 11H4.505V12.25H5.13H11H11.625V11H11H5.13Z"
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                                fill="currentColor"
-                            />
-                        </svg>
-                        <span>Download</span>
-                    </a>
-                    <div className={styles.innerbar}>
-                        <Time />
-                    </div>
+                    {outerBar}
+                    <div className={styles.innerbar}>{innerBar}</div>
                 </div>
             </div>
         </div>
