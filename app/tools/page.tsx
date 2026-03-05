626f4509ad74fee622ab3550b925d83dc5c80803
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-03-05 11:17:11 -0500

    feat: page stubs

---
+++ app/tools/page.tsx
@@ -0,0 +1,16 @@
+import styles from './page.module.scss';
+
+export default function Page() {
+    return (
+        <div className={styles.page}>
+            <div className={styles.pane}>
+                <div className={styles.list}>
+                    <div className={styles.input}>Input</div>
+                    <div className={styles.items}>List</div>
+                </div>
+                <div className={styles.view}>Viewer</div>
+            </div>
+            <div className={styles.statusbar}>Status bar</div>
+        </div>
+    );
+}
