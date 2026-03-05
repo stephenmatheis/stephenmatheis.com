c2f08ffcf3f0d28770c19017bd142600f0b20b88
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-03-05 16:41:43 -0500

    feat: scramble text (WIP)

--- components/Time/Time.tsx
+++ components/Time/Time.tsx
@@ -3,6 +3,37 @@
 import { useEffect, useState } from 'react';
 import styles from './Time.module.scss';
 
+const numerals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+const alphabet = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+];
+
 export function Time() {
     const [time, setTime] = useState<string>('MMM DD YY HH:MM');
 
