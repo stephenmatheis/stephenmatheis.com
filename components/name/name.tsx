6867a6781a7ba807eb4fb1d131f1b783200e310a
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-03-07 12:44:26 -0500

    feat: update skills

--- components/name/name.tsx
+++ components/name/name.tsx
@@ -1,12 +1,10 @@
-import { Section } from '@/components/section';
 import styles from './name.module.scss';
 
 export function Name() {
     return (
         <div className={styles.name}>
-            <h1>
-                Stephen Matheis <span>|</span> <span>Software Engineer</span>
-            </h1>
+            <h1>Stephen Matheis</h1>
+            <div className={styles.muted}>Software Engineer</div>
             <div className={styles.muted}>Washington, D.C.</div>
             <div className={styles.muted}>TS/SCI</div>
         </div>
