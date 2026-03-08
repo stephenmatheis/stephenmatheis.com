63d561f89b0c130580f8c7428b1f55cf5a2dc3a9
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-03-08 16:57:00 -0400

    fix: about

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -4,8 +4,8 @@ import styles from './About.module.scss';
 export function About() {
     return (
         <Section className={styles.about} heading="">
-            Full-stack developer. Holds an active TS/SCI clearance. 15 years of experience delivering software solutions
-            solutions for federal agencies. Expert in large distributed systems serving global critical.
+            Full-stack developer. Holds an active TS/SCI clearance. 15 years of experience writing software for federal
+            agencies. Expert in large distributed systems serving global critical.
         </Section>
     );
 }
