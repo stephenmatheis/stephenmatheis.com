2194b9edc109b0861f03c73c129899f28e4f2697
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-03-14 16:35:31 -0400

    feat: pros

--- components/ProsAndCons/ProsAndCons.tsx
+++ components/ProsAndCons/ProsAndCons.tsx
@@ -2,24 +2,37 @@
 
 import { useEffect, useState } from 'react';
 import { Statusbar } from '@/components/Statusbar';
+import { useMode } from '@/providers/ModeProvider';
 import { Time } from '@/components/Time';
+import { useVimMotions } from '@/hooks/useVimMotions';
 import styles from './ProsAndCons.module.scss';
 
 export function ProsAndCons() {
-    const [pros, setPros] = useState<string>('');
-    const [cons, setCons] = useState<string>('');
+    const { mode } = useMode();
+    const [pros, setPros] = useState<string[]>(['One', 'Two', 'Three', 'Four']);
+    const [cons, setCons] = useState<string[]>(['']);
+    const [selected, setSelected] = useState<number>(0);
+
+    useVimMotions({
+        max: pros.length - 1,
+        selected,
+        setSelected,
+        onEnter() {
+            console.log('???');
+        },
+    });
 
     useEffect(() => {
         const storedPros = localStorage.getItem('pros');
 
         if (storedPros !== null) {
-            setPros(storedPros);
+            setPros(JSON.parse(storedPros));
         }
 
         const storedCons = localStorage.getItem('cons');
 
         if (storedCons !== null) {
-            setCons(storedCons);
+            setPros(JSON.parse(storedCons));
         }
     }, []);
 
@@ -28,7 +41,48 @@ export function ProsAndCons() {
             <div className={styles.window}>
                 <div className={styles.pane}>
                     <div className={styles.label}>Pros</div>
-                    <textarea
+                    <div className={styles.editor}>
+                        {pros.map((pro, index) => {
+                            const lineNumber = index + 1;
+                            const isSelected = index === selected;
+
+                            return (
+                                <div
+                                    key={index}
+                                    className={`${styles.line} ${isSelected && mode === 'NORMAL' ? styles.selected : styles.normal}`}
+                                >
+                                    <span className={styles.number}>{lineNumber}</span>{' '}
+                                    {mode === 'INSERT' && isSelected ? (
+                                        <>
+                                            <input
+                                                autoFocus
+                                                type="text"
+                                                value={pro}
+                                                onKeyDown={(event) => {
+                                                    console.log('close and go to next');
+                                                }}
+                                                onChange={(event) => {
+                                                    setPros((prev) => {
+                                                        return prev.map((item, prevIndex) => {
+                                                            if (prevIndex === index) {
+                                                                return event.target.value;
+                                                            }
+
+                                                            return item;
+                                                        });
+                                                    });
+                                                }}
+                                            />
+                                            {/* <div className={styles.border} /> */}
+                                        </>
+                                    ) : (
+                                        <span className={mode === 'INSERT' ? styles.muted : styles.content}>{pro}</span>
+                                    )}
+                                </div>
+                            );
+                        })}
+                    </div>
+                    {/* <textarea
                         autoFocus
                         className={styles.editor}
                         value={pros}
@@ -36,18 +90,18 @@ export function ProsAndCons() {
                             setPros(event.target.value);
                             localStorage.setItem('pros', event.target.value);
                         }}
-                    />
+                    /> */}
                 </div>
                 <div className={styles.pane}>
                     <div className={styles.label}>Cons</div>
-                    <textarea
+                    {/* <textarea
                         className={styles.editor}
                         value={cons}
                         onChange={(event) => {
                             setCons(event.target.value);
                             localStorage.setItem('cons', event.target.value);
                         }}
-                    />
+                    /> */}
                 </div>
             </div>
             <Statusbar msg="" outerBar="" innerBar={<Time />} />
