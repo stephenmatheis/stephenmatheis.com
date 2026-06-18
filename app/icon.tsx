5d9924553f841e991c79e4671aa078becabbdc79
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-06-18 18:29:57 -0400

    feat: font grid

--- app/icon.tsx
+++ app/icon.tsx
@@ -10,17 +10,16 @@ export default function Icon() {
     return new ImageResponse(
         <div
             style={{
-                fontSize: 24,
-                background: 'black',
+                fontSize: 20,
                 width: '100%',
                 height: '100%',
                 display: 'flex',
                 alignItems: 'center',
                 justifyContent: 'center',
-                color: 'white',
+                color: 'black',
             }}
         >
-            _
+            {'///'}
         </div>,
         {
             ...size,
