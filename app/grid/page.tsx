7800083879ef434b369385a52882fb61b22eb929
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-06-19 08:17:09 -0400

    feat: dom page

--- app/grid/page.tsx
+++ app/grid/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.scss';
 
 export default function Home() {
     return (
-        <div className={styles.page} style={{ opacity: 0 }}>
+        <div className={styles.page}>
             <div>Departure Mono</div>
             <div className={styles.text} style={{ fontFamily: 'var(--font-departure-mono)', fontSize: '33px' }}>
                 {departureMonoCodepoints.map((codepoint, i) => {
