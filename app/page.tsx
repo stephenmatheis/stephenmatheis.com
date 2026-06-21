8974ba8d638b5b4315748a96e9a0155bf964c6ab
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-06-21 00:19:55 -0400

    feat: textarea

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,49 @@
-import Link from 'next/link';
+'use client';
+
+import { useEffect, useRef } from 'react';
+import { Editor, Box } from '@/lib/editor';
+import { log, loggingOff } from '@/lib/utils';
 import styles from './page.module.scss';
 
 export default function Home() {
+    const pageRef = useRef<HTMLDivElement>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        loggingOff();
+
+        const editor = Editor({
+            canvas: canvasRef.current!,
+            textarea: textareaRef.current!,
+            container: containerRef.current!,
+        });
+
+        editor.root.add(Box({ flexDirection: 'column', paddingX: 2, paddingY: 1 }));
+
+        log('END');
+
+        if (pageRef.current) {
+            pageRef.current.style.opacity = '1';
+        }
+
+        return () => editor.destroy();
+    }, []);
+
     return (
-        <div className={styles.page}>
-            <h1>Routes</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/canvas">/canvas</Link>
-                    </li>
-                    <li>
-                        <Link href="/dom">/dom</Link>
-                    </li>
-                    <li>
-                        <Link href="/grid">/grid</Link>
-                    </li>
-                </ul>
-            </nav>
+        <div ref={pageRef} className={styles.page} style={{ opacity: 0 }}>
+            <div ref={containerRef} className={styles.renderer}>
+                <canvas ref={canvasRef} />
+                <textarea
+                    ref={textareaRef}
+                    className={styles.input}
+                    name="input"
+                    tabIndex={0}
+                    autoFocus
+                    suppressHydrationWarning
+                />
+            </div>
         </div>
     );
 }
