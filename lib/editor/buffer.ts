dcaef9132827068c7014147d402febc4d505a72f
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-06-21 15:04:15 -0400

    fix: backspace

--- lib/editor/buffer.ts
+++ lib/editor/buffer.ts
@@ -10,12 +10,14 @@ export type BufferProps = {
 export function Buffer({ state, actions }: BufferProps) {
     function writeChar(char: string) {
         if (state.cursor.y < state.rows && state.cursor.x < state.cols) {
-            state.chars[state.cursor.y][state.cursor.x] = char;
-
             const r = state.activeRegion;
             const maxX = r ? r.x + r.width - 1 : state.cols - 1;
             const minX = r ? r.x : 0;
             const maxY = r ? r.y + r.height - 1 : state.rows - 1;
+
+            if (state.cursor.x === maxX) return;
+
+            state.chars[state.cursor.y][state.cursor.x] = char;
 
             if (state.cursor.x >= maxX && state.cursor.y < maxY) {
                 state.cursor = { x: minX, y: state.cursor.y + 1 };
