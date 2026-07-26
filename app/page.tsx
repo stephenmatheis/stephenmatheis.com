b101fa170778fc4e19195c57b6748e94ef57e838
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-07-25 23:45:36 -0400

    feat: github link

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+
 export default function Home() {
-    return <pre>Hello, world.</pre>;
+    return (
+        <>
+            <pre>
+                Hello, there. <Link href="https://github.com/stephenmatheis">{"Here's some of my work"}.</Link>
+            </pre>
+        </>
+    );
 }
