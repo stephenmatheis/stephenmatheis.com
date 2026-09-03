1f19b383d2bbef7eb1c844c06d4cd3e3fe0ca651
Stephen Matheis <stephen.a.matheis@gmail.com>
2026-09-03 13:54:21 -0400

    feat: boot sequence

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
             <header>
                 <h1>STEPHEN MATHEIS</h1>
                 <h2>THINKING MEAT</h2>
-                <h3>1988-{new Date().getFullYear()}</h3>
+                <h3>IN SERVICE 1988-{new Date().getFullYear()}</h3>
             </header>
             <main>
                 <Prompt />
